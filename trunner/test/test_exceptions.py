819c9c37f9cbd7f25c9c019e4196b9cfaf7758f6
Jakub Sarzyński <jakub.sarzynski@phoenix-rtos.com>
2021-04-29 11:47:35 +0200

    runner: add tests for pyexpect exceptions handling

--- trunner/test/test_exceptions.py
+++ trunner/test/test_exceptions.py
@@ -1,6 +1,9 @@
+import os
 import re
+import threading
 
 import pytest
+import pexpect.fdpexpect
 
 from trunner.config import ALL_TARGETS
 from trunner.testcase import TestCase
@@ -9,6 +12,96 @@ from trunner.testcase import TestCase
 # Pytest tries to collect TestCase as a class to test
 # Mark TestCase as not testable
 TestCase.__test__ = False
+
+
+class TestPexpectException:
+    """ Tests check if PyExpect exceptions are handled properly """
+
+    FIFO = "./test_exceptions.fifo"
+
+    @pytest.fixture
+    def testcase(self):
+        return TestCase(
+            name='xyz',
+            target=ALL_TARGETS[0],
+            timeout=3,
+            exec_bin='xyz'
+        )
+
+    @pytest.fixture(scope="class")
+    def fifo(self):
+        os.mkfifo(self.FIFO)
+        yield
+        os.unlink(self.FIFO)
+
+    @staticmethod
+    def fake_harness(proc):
+        proc.expect_exact("Hello ")
+        proc.expect_exact("world!")
+
+    def test_timeout(self, fifo, testcase):
+        def writer(lock):
+            with open(self.FIFO, 'w') as f:
+                f.write("Hello universe!")
+                f.flush()
+
+                # We've written what we wanted to
+                # Keep open fifo to not trigger EOF, hanging on a lock
+                lock.acquire()
+
+        lock = threading.Lock()
+        lock.acquire()
+
+        thread = threading.Thread(target=writer, args=(lock,))
+        thread.start()
+
+        fifo_r = open(self.FIFO, 'r')
+
+        proc = pexpect.fdpexpect.fdspawn(fifo_r, encoding='utf-8', timeout=1)
+        testcase.harness = TestPexpectException.fake_harness
+        testcase.handle(proc, psh=False)
+
+        # Release lock for writer to close fifo
+        lock.release()
+        thread.join()
+        fifo_r.close()
+
+        assert testcase.failed()
+        assert "EXCEPTION TIMEOUT" in testcase.exception
+
+        # We should have read word "Hello "
+        # Check if read/expect buffers content are mentioned in the exception
+        assert "Hello " not in testcase.exception
+        assert "world!" in testcase.exception
+        assert "universe!" in testcase.exception
+
+    def test_eof(self, fifo, testcase):
+        def writer():
+            with open(self.FIFO, 'w') as f:
+                f.write("Hello universe!")
+
+        thread = threading.Thread(target=writer)
+        thread.start()
+
+        fifo_r = open(self.FIFO, 'r')
+
+        # Wait for end of write to get EOF
+        thread.join()
+
+        proc = pexpect.fdpexpect.fdspawn(fifo_r, encoding='utf-8')
+        testcase.harness = TestPexpectException.fake_harness
+        testcase.handle(proc, psh=False)
+
+        fifo_r.close()
+
+        assert testcase.failed()
+        assert "EXCEPTION EOF" in testcase.exception
+
+        # We should have read word "Hello "
+        # Check if read/expect buffers content are mentioned in the exception
+        assert "Hello " not in testcase.exception
+        assert "world!" in testcase.exception
+        assert "universe!" in testcase.exception
 
 
 class TestExceptionHandler:
