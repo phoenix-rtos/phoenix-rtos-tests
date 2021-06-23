0ef9a977dfaacb863062447dcccd9213b4c42013
Marek Białowąs <marek.bialowas@phoenix-rtos.com>
2021-06-23 17:57:15 +0200

    libc: add libc unit tests framework stub
    JIRA: DTR-75

---
+++ libc/main.c
@@ -0,0 +1,29 @@
+/*
+ * Phoenix-RTOS
+ *
+ * libc-tests
+ *
+ * Main entry point.
+ *
+ * Copyright 2021 Phoenix Systems
+ * Author: Marek Bialowas
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#include "unity_fixture.h"
+
+/* no need for forward declarations, RUN_TEST_GROUP does it by itself */
+void runner(void)
+{
+	//RUN_TEST_GROUP(resolve_path);
+}
+
+
+int main(int argc, char *argv[])
+{
+	UnityMain(argc, (const char **)argv, runner);
+	return 0;
+}
