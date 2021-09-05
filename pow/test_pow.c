95fede13cff00ad6c3676e59591ccdbcfdc0b924
mwalczyk <umwalczyk@gmail.com>
2021-09-05 21:10:13 +0200

    Update test_pow.c
    Test cases added based on conditions in power.c in libphoenix directory

--- pow/test_pow.c
+++ pow/test_pow.c
@@ -15,106 +15,146 @@ TEST_TEAR_DOWN(test_pow)
 	errno = 0;
 }
 
-TEST(test_pow, tc1_normal_range_base_0_and_exponent_0)
+TEST(test_pow, tc1_base_0_and_exponent_0)
 {
-	double expected = 1;
 	double result = pow(0.0, 0.0);
-	TEST_ASSERT_EQUAL_DOUBLE(expected,  result);
+	TEST_ASSERT_EQUAL_DOUBLE(1.0,  result);
+	TEST_ASSERT_EQUAL_INT(0, errno);
+	
+	result = pow(0.0, -0.0);
+	TEST_ASSERT_EQUAL_DOUBLE(1.0,  result);
+	TEST_ASSERT_EQUAL_INT(0, errno);
+	
+	result = pow(-0.0, 0.0);
+	TEST_ASSERT_EQUAL_DOUBLE(1.0,  result);
+	TEST_ASSERT_EQUAL_INT(0, errno);
+	
+	result = pow(-0.0, -0.0);
+	TEST_ASSERT_EQUAL_DOUBLE(1.0,  result);
 	TEST_ASSERT_EQUAL_INT(0, errno);
 	
 }
 
-TEST(test_pow, tc1_1_normal_range_base_0_exponent_less_than_0)
+TEST(test_pow, tc2_base_0_exponent_less_than_0)
 {
-	double expected = INFINITY;
 	double result = pow(0.0, -1.0);
-	TEST_ASSERT_EQUAL_DOUBLE(expected,  result);
+	TEST_ASSERT_EQUAL_DOUBLE(INFINITY,  result);
+	TEST_ASSERT_EQUAL_INT(0, errno);
+	
+	result = pow(-0.0, -1.0);
+	TEST_ASSERT_EQUAL_DOUBLE(-INFINITY,  result);
 	TEST_ASSERT_EQUAL_INT(0, errno);
 	
 }
 
-TEST(test_pow, tc2_normal_range_base_bigger_than_0_exponent_equal_zero)
+TEST(test_pow, tc3_base_bigger_than_0_exponent_equal_zero)
 {
-	double expected = 1.0;
 	double result = pow(1.0, 0.0);
-	
 	TEST_ASSERT_EQUAL_INT(0, errno);
-	TEST_ASSERT_EQUAL_DOUBLE(expected, result);
+	TEST_ASSERT_EQUAL_DOUBLE(1, result);
 	
-}
-
-TEST(test_pow, tc3_normal_range_base_equal_1_exponent_double_max)
-{
-	double expected = 1.0;
-	double result = pow(1.0, DBL_MAX);
-	
+	result = pow(1.0, -0.0);
 	TEST_ASSERT_EQUAL_INT(0, errno);
-	TEST_ASSERT_EQUAL_DOUBLE(expected, result);
+	TEST_ASSERT_EQUAL_DOUBLE(1, result);
 	
-	
-	result = pow(1.0, -DBL_MAX);
-	
+	result = pow(DBL_MAX, 0.0);
 	TEST_ASSERT_EQUAL_INT(0, errno);
-	TEST_ASSERT_EQUAL_DOUBLE(expected, result);
+	TEST_ASSERT_EQUAL_DOUBLE(1, result);
+	
+	result = pow(-DBL_MAX, 0.0);
+	TEST_ASSERT_EQUAL_INT(0, errno);
+	TEST_ASSERT_EQUAL_DOUBLE(1, result);
+	
 	
 }
 
-/*
-TEST(test_pow, tc1_domain_error_expected)
+TEST(test_pow, tc4_domain_error)
 {
-	double expected = INFINITY;
-	double result = pow(0.0, -10.0);
+	//double result = pow(-1.0, 0.5);
 	
-	TEST_ASSERT_EQUAL_INT(EDOM, errno);
-	TEST_ASSERT_EQUAL_DOUBLE(expected, result);
+	//TEST_ASSERT_EQUAL_INT(EDOM, errno);
+	//TEST_ASSERT_EQUAL_DOUBLE(NAN, result);
+	
+	
+	//result = pow(-DBL_MAX, -INT_MAX-0.5);
+	//TEST_ASSERT_EQUAL_INT(EDOM, errno);
+	//TEST_ASSERT_EQUAL_DOUBLE(NAN, result);
 }
 
 
-TEST(test_pow, tc3_domain_error_expected)
+TEST(test_pow, tc5_negative_and_positive_exponent_intiger)
+{
+	double result = pow(-1.0, 3);
+	TEST_ASSERT_EQUAL_INT(0, errno);
+	TEST_ASSERT_EQUAL_DOUBLE(-1.0, result);
+	
+	result = pow(-1.0, 2);
+	TEST_ASSERT_EQUAL_INT(0, errno);
+	TEST_ASSERT_EQUAL_DOUBLE(1.0, result);
+}
+
+
+TEST(test_pow, tc6_exponent_max_value)
 {
 	
-	double result = pow(0.0, -DBL_MAX);
-	TEST_ASSERT_EQUAL_INT(EDOM, errno);
+	double result = pow(1.0, INT_MAX);
+	TEST_ASSERT_EQUAL_INT(0, errno);
+	TEST_ASSERT_EQUAL_DOUBLE(1, result);
+	
+	result = pow(1.0, INT_MIN);
+	TEST_ASSERT_EQUAL_INT(0, errno);
+	TEST_ASSERT_EQUAL_DOUBLE(1, result);
+	
+	//result = pow(2.0, INT_MAX);
+	//TEST_ASSERT_EQUAL_INT(ERANGE, errno);
+	//TEST_ASSERT_EQUAL_DOUBLE(INFINITY, result);
+	
+	//result = pow(2.0, INT_MIN);
+	//TEST_ASSERT_EQUAL_INT(ERANGE, errno);
+	//TEST_ASSERT_EQUAL_DOUBLE(0, result);
 }
 
-TEST(test_pow, tc4_domain_error_expected)
+TEST(test_pow, tc7_exponent_bigger_than_max_value)
 {	
-	double result = pow(-1.0, 0.5);
-	TEST_ASSERT_EQUAL_INT(EDOM, errno);
+	double result = pow(1.0, INT_MAX+1);
+	TEST_ASSERT_EQUAL_INT(0, errno);
+	TEST_ASSERT_EQUAL_DOUBLE(1, result);
 }
 
-TEST(test_pow, tc5_domain_error_expected)
-{
-	double result = pow(-1.0 , DBL_MAX-0.5);
-	TEST_ASSERT_EQUAL_INT(EDOM, errno);
-}
-
-TEST(test_pow, tc6_domain_error_expected)
+TEST(test_pow, tc8_normal_range)
 {	
-	double result = pow(-DBL_MAX, 0.5);
-	TEST_ASSERT_EQUAL_INT(EDOM, errno);
+	double result = pow(10.0, 2);
+	TEST_ASSERT_EQUAL_INT(0, errno);
+	TEST_ASSERT_EQUAL_DOUBLE(100.0, result);
+	
+	result = pow(2, 2);
+	TEST_ASSERT_EQUAL_INT(0, errno);
+	TEST_ASSERT_EQUAL_DOUBLE(4.0, result);
+	
+	result = pow(2, 3);
+	TEST_ASSERT_EQUAL_INT(0, errno);
+	TEST_ASSERT_EQUAL_DOUBLE(8.0, result);
+	
+	result = pow(10.0, 2);
+	TEST_ASSERT_EQUAL_INT(0, errno);
+	TEST_ASSERT_EQUAL_DOUBLE(100.0, result);
+	
+	result = pow(10.0, 2);
+	TEST_ASSERT_EQUAL_INT(0, errno);
+	TEST_ASSERT_EQUAL_DOUBLE(100.0, result);
 }
 
-TEST(test_pow, tc7_domain_error_expected)
-	
-	double result = pow(-1.0, -DBL_MAX + 0.5);
-	TEST_ASSERT_EQUAL_INT(EDOM, errno);
-}
-*/
 
 TEST_GROUP_RUNNER(test_pow)
 {
-	//RUN_TEST_CASE(test_pow, tc1_domain_error_expected);
-	//RUN_TEST_CASE(test_pow, tc3_domain_error_expected);
-	//RUN_TEST_CASE(test_pow, tc4_domain_error_expected);
-	//RUN_TEST_CASE(test_pow, tc5_domain_error_expected);
-	//RUN_TEST_CASE(test_pow, tc6_domain_error_expected);
-	//RUN_TEST_CASE(test_pow, tc7_domain_error_expected);
-	RUN_TEST_CASE(test_pow, tc1_normal_range_base_0_and_exponent_0);
-	RUN_TEST_CASE(test_pow, tc1_1_normal_range_base_0_exponent_less_than_0);
-	RUN_TEST_CASE(test_pow, tc2_normal_range_base_bigger_than_0_exponent_equal_zero);
-	RUN_TEST_CASE(test_pow, tc3_normal_range_base_equal_1_exponent_double_max);
-//	RUN_TEST_CASE(test_pow, out_range_base_and_exponend_equal_DBL_MAX);
+	RUN_TEST_CASE(test_pow, tc1_base_0_and_exponent_0);
+	RUN_TEST_CASE(test_pow, tc2_base_0_exponent_less_than_0);
+	RUN_TEST_CASE(test_pow, tc3_base_bigger_than_0_exponent_equal_zero);
+	RUN_TEST_CASE(test_pow, tc4_domain_error);
+	RUN_TEST_CASE(test_pow, tc5_negative_and_positive_exponent_intiger);
+	RUN_TEST_CASE(test_pow, tc6_exponent_max_value);
+	RUN_TEST_CASE(test_pow, tc7_exponent_bigger_than_max_value);
+	RUN_TEST_CASE(test_pow, tc8_normal_range);
 }
 
 void runner(void)
