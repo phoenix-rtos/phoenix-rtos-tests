d041ec2757af22b6476cac2845036e69e3d2fb7d
Damian Loewnau <damian.loewnau@phoenix-rtos.com>
2022-02-11 16:54:41 +0100

    runner: add armv7a9-zynq7000-qemu implementation
    JIRA: CI-110

--- trunner/runners/QemuRunner.py
+++ trunner/runners/QemuRunner.py
@@ -1,7 +1,7 @@
 #
 # Phoenix-RTOS test runner
 #
-# ia32-generic qemu emulator runner
+# qemu emulator runner
 #
 # Copyright 2021 Phoenix SYstems
 # Authors: Jakub Sarzyński, Mateusz Niewiadomski, Damian Loewnau
@@ -11,14 +11,19 @@ import pexpect
 import signal
 
 from .common import Runner
-
+from trunner.config import PHRTOS_PROJECT_DIR
 
 class QemuRunner(Runner):
     """This class provides interface to run test case using QEMU as a device."""
 
-    def __init__(self, qemu, log):
+    QEMU_CMD = {
+        'ia32-generic': f'{PHRTOS_PROJECT_DIR}/scripts/ia32-generic-test.sh',
+        'armv7a9-zynq7000-qemu': f'{PHRTOS_PROJECT_DIR}/scripts/armv7a9-zynq7000.sh'
+    }
+
+    def __init__(self, target, log):
         super().__init__(log)
-        self.qemu = qemu
+        self.cmd = self.QEMU_CMD[target]
 
     def flash(self):
         """No-op method for QemuRunner, forced inheritance from Runner class"""
@@ -28,7 +33,7 @@ class QemuRunner(Runner):
         if test.skipped():
             return
 
-        proc = pexpect.spawn(self.qemu, encoding='ascii', timeout=test.timeout)
+        proc = pexpect.spawn(self.cmd, encoding='ascii', timeout=test.timeout)
         if self.logpath:
             proc.logfile = open(self.logpath, "a")
 
