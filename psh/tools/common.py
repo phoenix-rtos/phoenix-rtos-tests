af89f08be13726efc5147d58526d98531746881c
Damian Loewnau <damian.loewnau@phoenix-rtos.com>
2022-09-14 18:16:58 +0200

    psh: add test-touch
    JIRA: PD-77

--- psh/tools/common.py
+++ psh/tools/common.py
@@ -17,12 +17,10 @@ import string
 
 import psh.tools.psh as psh
 
-# according to control sequence introducer pattern
-CONTROL_CODE = r'(\x1b\[[\x30-\x3F]*[\x20-\x2F]*[\x40-\x7E])'
-OPTIONAL_CONTROL_CODE = CONTROL_CODE + r'?'
+from psh.tools.psh import CONTROL_CODE
+
 # acceptable separators: white spaces (wss) + CC, CC + wss, wss
 SEPARATOR_PATTERN = r'(?:' + CONTROL_CODE + r'|\s)+'
-
 CHARS = list(set(string.printable) - set(string.whitespace) - set('/'))
 
 
@@ -38,3 +36,69 @@ def create_testdir(p, dirname):
                      'Try to re-build the project and run specified test second time.'])
 
     psh.assert_cmd(p, f'mkdir {dirname}', '', msg)
+
+
+def assert_present(name, files, dir=False):
+    ''' Asserts that the `name` file/directory is present in the `files` named tuple'''
+    for file in files:
+        if name == file.name:
+            # TODO: enable after adding multiple user support
+            # assert file.owner == 'root', f'{name} not owned by root'
+            if dir:
+                assert file.is_dir, f'{name} is not directory'
+            break
+    else:
+        assert False, f'failed to find {name}'
+
+
+def _assert_created(p, name, dir=False):
+    ''' Asserts that the `name` file/directory is properly created
+    name can also be the file/directory absolute path'''
+
+    cmd = 'mkdir' if dir else 'touch'
+    psh.assert_cmd(p, f'{cmd} {name}', msg=f'Failed to create: {name} using {cmd}')
+
+    path_elements = name.rsplit('/', 1)
+    if len(path_elements) == 2:
+        name = path_elements[1]
+        path = path_elements[0]
+    else:
+        path = '/'
+
+    files = psh.ls(p, path)
+    assert_present(name, files, dir=dir)
+
+
+def assert_file_created(p, name):
+    _assert_created(p, name, dir=False)
+
+
+def assert_dir_created(p, name):
+    _assert_created(p, name, dir=True)
+
+
+def _assert_random(p, pool, path, count, dir=False):
+    ''' Asserts that `count` number of files/directories with random names
+    (created by matching chars from the `pool` list)
+    are properly created in the `path` directory'''
+    psh.assert_cmd(p, f'mkdir {path}', msg=f'Failed to create {path} directory')
+    names = get_rand_strings(pool, count)
+
+    cmd = 'mkdir' if dir else 'touch'
+    for name in names:
+        psh.assert_cmd(p, f'{cmd} {path}/{name}', msg=f'Failed to create {name}')
+
+    files = psh.ls(p, path)
+    msg = f'The number of created random files/directories is wrong ({len(files)} != {len(names) + 2})'
+    # +2 because of . and ..
+    assert len(files) == len(names) + 2, msg
+
+    assert_present(name, files, dir=dir)
+
+
+def assert_random_files(p, pool, path, count=20):
+    _assert_random(p, pool, path, count, dir=False)
+
+
+def assert_random_dirs(p, pool, path, count=20):
+    _assert_random(p, pool, path, count, dir=True)
