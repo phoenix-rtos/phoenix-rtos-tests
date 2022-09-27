14ddeb9740b06fa7576fb7cfd72bbf6821afab47
DawidSzpejna <dawid.szpejna13@gmail.com>
2022-09-27 16:34:30 +0200

    unity: enable Double Precision
    Tests for strtod need macros accept double type.
    
    JIRA: RTOS-219

--- unity/unity_config.h
+++ unity/unity_config.h
@@ -2,6 +2,7 @@
 #define UNITY_CONFIG_H
 
 #define UNITY_FIXTURE_NO_EXTRAS
+#define UNITY_INCLUDE_DOUBLE
 
 /* force support for 64-bit integer assertions even on 32-bit targets */
 #define UNITY_SUPPORT_64
