3f49a17ae90faedb5b7f52a3947acaddc3832fd7
Marek Białowąs <marek.bialowas@phoenix-rtos.com>
2022-10-07 11:01:18 +0200

    libc: fix programming mistakes
    This makes tests compilable on host (linux) and runnable (no SIGSEGV).
    
    JIRA: CI-100

--- libc/string.c
+++ libc/string.c
@@ -26,16 +26,13 @@
 
 
 /* fixtures for strlcpy */
-char source[SRC_SIZE];
-char dest[DEST_SIZE];
+static char source[SRC_SIZE];
+static char dest[DEST_SIZE];
 
 /* fixtures for strlcat */
-char source1[SRC1_SIZE];
-char source2[SRC2_SIZE];
-char buffer[BUF_SIZE];
-
-/* common fixtures */
-int retval;
+static char source1[SRC1_SIZE];
+static char source2[SRC2_SIZE];
+static char buffer[BUF_SIZE];
 
 
 TEST_GROUP(string_strlcpy);
@@ -53,10 +50,12 @@ TEST_TEAR_DOWN(string_strlcpy)
 }
 
 
+/* NOTE: strlcpy not available on linux, enable tests only on phoenix */
+#ifdef __phoenix__
 TEST(string_strlcpy, strlcpy_fullcopy)
 {
 	/* Test full copy */
-	retval = strlcpy(dest, source, sizeof(source));
+	int retval = strlcpy(dest, source, sizeof(source));
 	TEST_ASSERT_EQUAL_INT(sizeof(source) - 1, retval);
 	TEST_ASSERT_EQUAL_STRING(source, dest);
 }
@@ -65,7 +64,7 @@ TEST(string_strlcpy, strlcpy_fullcopy)
 TEST(string_strlcpy, strlcpy_shorter)
 {
 	/* Test shorter than source copy */
-	retval = strlcpy(dest, source, sizeof(source) - 2);
+	int retval = strlcpy(dest, source, sizeof(source) - 2);
 	TEST_ASSERT_EQUAL_STRING("ab", dest);
 	TEST_ASSERT_EQUAL_INT(sizeof(source) - 1, retval);
 }
@@ -75,7 +74,7 @@ TEST(string_strlcpy, strlcpy_longer)
 {
 	/* Test longer than source copy */
 	source[3] = '\0'; /* source is now "abc" null terminated; */
-	retval = strlcpy(dest, source, sizeof(source));
+	int retval = strlcpy(dest, source, sizeof(source));
 	TEST_ASSERT_EQUAL_STRING("abc", dest);
 	TEST_ASSERT_EQUAL_INT(sizeof(source) - 2, retval);
 	source[3] = 'd';
@@ -85,7 +84,7 @@ TEST(string_strlcpy, strlcpy_longer)
 TEST(string_strlcpy, strlcpy_onelength)
 {
 	/* Test 1 length copy */
-	retval = strlcpy(dest, source, 1);
+	int retval = strlcpy(dest, source, 1);
 	TEST_ASSERT_EQUAL_STRING("", dest);
 	TEST_ASSERT_EQUAL_INT(sizeof(source) - 1, retval);
 }
@@ -94,19 +93,23 @@ TEST(string_strlcpy, strlcpy_onelength)
 TEST(string_strlcpy, strlcpy_zerolength)
 {
 	/* Test 0 length copy */
-	retval = strlcpy(dest, source, 0);
+	int retval = strlcpy(dest, source, 0);
 	TEST_ASSERT_EQUAL_STRING("xxxx", dest);
 	TEST_ASSERT_EQUAL_INT(sizeof(source) - 1, retval);
 }
 
+#endif /* __phoenix__ */
+
 
 TEST_GROUP_RUNNER(string_strlcpy)
 {
+#ifdef __phoenix__
 	RUN_TEST_CASE(string_strlcpy, strlcpy_fullcopy);
 	RUN_TEST_CASE(string_strlcpy, strlcpy_shorter);
 	RUN_TEST_CASE(string_strlcpy, strlcpy_longer);
 	RUN_TEST_CASE(string_strlcpy, strlcpy_onelength);
 	RUN_TEST_CASE(string_strlcpy, strlcpy_zerolength);
+#endif
 }
 
 
@@ -126,12 +129,13 @@ TEST_TEAR_DOWN(string_strlcat)
 }
 
 
+#ifdef __phoenix__
 TEST(string_strlcat, strlcat_fullconcat_empty)
 {
 	memset(buffer, '\0', sizeof(buffer));
 
 	/* Normal, full concat to empty string */
-	retval = strlcat(buffer, source1, sizeof(buffer));
+	int retval = strlcat(buffer, source1, sizeof(buffer));
 	TEST_ASSERT_EQUAL_INT(3, retval);
 	TEST_ASSERT_EQUAL_STRING(source1, buffer);
 }
@@ -142,7 +146,7 @@ TEST(string_strlcat, strlcat_fullconcat_part)
 	buffer[3] = '\0';
 
 	/* Normal full concat to partially filled string */
-	retval = strlcat(buffer, source2, sizeof(buffer));
+	int retval = strlcat(buffer, source2, sizeof(buffer));
 	TEST_ASSERT_EQUAL_INT(sizeof(source2) + 2, retval);
 	TEST_ASSERT_EQUAL_STRING("klmdefgh", buffer);
 }
@@ -153,7 +157,7 @@ TEST(string_strlcat, strlcat_partconcat_overflow)
 	buffer[8] = '\0';
 
 	/* Partial concat to partially filled string that should overflow the buffer */
-	retval = strlcat(buffer, source2, sizeof(buffer));
+	int retval = strlcat(buffer, source2, sizeof(buffer));
 	TEST_ASSERT_EQUAL_INT(sizeof(buffer) + 1, retval);
 	TEST_ASSERT_EQUAL_STRING("klmnopqrdef", buffer);
 }
@@ -163,7 +167,7 @@ TEST(string_strlcat, strlcat_onelength)
 {
 	/* 1 length concat */
 	buffer[6] = '\0';
-	retval = strlcat(buffer, source2, 1);
+	int retval = strlcat(buffer, source2, 1);
 	TEST_ASSERT_EQUAL_INT(sizeof(source2), retval);
 	TEST_ASSERT_EQUAL_STRING("klmnop", buffer);
 }
@@ -173,17 +177,20 @@ TEST(string_strlcat, strlcat_zerolength)
 {
 	/* 0 length concat */
 	buffer[6] = '\0';
-	retval = strlcat(buffer, source2, 0);
+	int retval = strlcat(buffer, source2, 0);
 	TEST_ASSERT_EQUAL_INT(sizeof(source2) - 1, retval);
 	TEST_ASSERT_EQUAL_STRING("klmnop", buffer);
 }
 
+#endif /* __phoenix__ */
 
 TEST_GROUP_RUNNER(string_strlcat)
 {
+#ifdef __phoenix__
 	RUN_TEST_CASE(string_strlcat, strlcat_fullconcat_empty);
 	RUN_TEST_CASE(string_strlcat, strlcat_fullconcat_part);
 	RUN_TEST_CASE(string_strlcat, strlcat_partconcat_overflow);
 	RUN_TEST_CASE(string_strlcat, strlcat_onelength);
 	RUN_TEST_CASE(string_strlcat, strlcat_zerolength);
+#endif
 }
