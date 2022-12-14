5324d6de04dcbf66f54649e39bd04d7c51c5f5a2
Damian Loewnau <damian.loewnau@phoenix-rtos.com>
2022-12-14 14:14:39 +0100

    unistd: unistd_file: correct and make POSIX/host compatible
    JIRA: PD-258

--- libc/main.c
+++ libc/main.c
@@ -21,24 +21,24 @@ void runner(void)
 {
 	setenv("POSIXLY_CORRECT", "y", 1);
 
-	// RUN_TEST_GROUP(stdio_fopenfclose);
-	// RUN_TEST_GROUP(stdio_line);
-	// RUN_TEST_GROUP(stdio_getput);
-	// RUN_TEST_GROUP(stdio_fileseek);
-	// RUN_TEST_GROUP(stdio_fileop);
-	// RUN_TEST_GROUP(stdio_bufs);
+	RUN_TEST_GROUP(stdio_fopenfclose);
+	RUN_TEST_GROUP(stdio_line);
+	RUN_TEST_GROUP(stdio_getput);
+	RUN_TEST_GROUP(stdio_fileseek);
+	RUN_TEST_GROUP(stdio_fileop);
+	RUN_TEST_GROUP(stdio_bufs);
 
-	// RUN_TEST_GROUP(getpwd);
-	// RUN_TEST_GROUP(resolve_path);
-	// RUN_TEST_GROUP(file);
-	// RUN_TEST_GROUP(unistd_getopt);
-	// RUN_TEST_GROUP(unistd_uids);
-	// RUN_TEST_GROUP(string_strlcpy);
-	// RUN_TEST_GROUP(string_strlcat);
-	// RUN_TEST_GROUP(unistd_fsdir);
+	RUN_TEST_GROUP(getpwd);
+	RUN_TEST_GROUP(resolve_path);
+	RUN_TEST_GROUP(file);
+	RUN_TEST_GROUP(unistd_getopt);
+	RUN_TEST_GROUP(unistd_uids);
+	RUN_TEST_GROUP(string_strlcpy);
+	RUN_TEST_GROUP(string_strlcat);
+	RUN_TEST_GROUP(unistd_fsdir);
 	RUN_TEST_GROUP(unistd_file);
-	// RUN_TEST_GROUP(wchar_wcscmp);
-	// RUN_TEST_GROUP(test_pthread_cond);
+	RUN_TEST_GROUP(wchar_wcscmp);
+	RUN_TEST_GROUP(test_pthread_cond);
 
 	unsetenv("POSIXLY_CORRECT");
 }
