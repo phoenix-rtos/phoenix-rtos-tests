224dbe0cf527b6fc7d218d3c58b3a44e9543258e
Jakub Sarzyński <jakub.sarzynski@phoenix-rtos.com>
2023-01-27 16:30:51 +0100

    extract 3rd party harnesses from the trunner
    JIRA: CI-162

---
+++ micropython/micropython.py
@@ -0,0 +1,129 @@
+import re
+from pathlib import Path
+
+from trunner.types import TestResult
+
+PROMPT = r"(\r+)\x1b\[0J" + r"\(psh\)% "
+EOL = r"\r+\n"
+
+UPYTH_PROMPT = ">>>"
+MICROPYTHON = "/bin/micropython"
+
+TESTS_WITH_REGEX_OUTPUT = {
+    "micropython/meminfo.py",
+    "basics/bytes_compare3.py",
+    "basics/builtin_help.py",
+    "thread/thread_exc2.py",
+    "esp32/partition_ota.py",
+    "cmdline/cmd_parsetree.py",
+    "cmdline/cmd_showbc.py",
+    "cmdline/cmd_verbose.py",
+}
+
+
+def standard_regex_escape(text):
+    """In tests with regex output syntax character \\ makes next char to be interpreted as a regex character"""
+
+    result = []
+    escape = False
+    for c in text:
+        if c == "\r":
+            continue
+        if c == "\n":
+            result.append(EOL)
+        elif escape:
+            escape = False
+            result.append(c)
+        elif c == "\\":
+            escape = True
+        elif c in ("(", ")", "[", "]", "{", "}", ".", "*", "+", "^", "$", "?", "|"):
+            result.append("\\" + c)
+        else:
+            result.append(c)
+
+    return "".join(result)
+
+
+def create_regex(text):
+    """Tests with regex output use special syntax for expected outputs"""
+
+    text = standard_regex_escape(text)
+
+    # ######## matches zero or more lines with text
+    text = text.replace(f"########{EOL}.+", "(.|\n)*")
+    text = text.replace(f"{EOL}########", "(.|\n)*")
+    text = text.replace("########", "(.|\n)*")
+
+    return text
+
+
+def is_test_with_regex_output(test_path: str):
+    """Some tests needs special handling - their expected output is a regex, not just string"""
+
+    p = Path(test_path)
+    return p.parent.name + "/" + p.name in TESTS_WITH_REGEX_OUTPUT
+
+
+def get_test(dut):
+    dut.expect(PROMPT, timeout=45)
+    output = dut.before
+    # Leave prompt in the buffer
+    dut.sendline()
+
+    re_result = re.search(r"Error: (.+) - (.+)", output)
+    assert (
+        re_result is None
+    ), f"There was an error {re_result.group(2)} during program {re_result.group(1)} execution:\n{output}\n"
+
+    # Looking for a path to the test
+    re_result = re.search(r"Running test: (/.+\.py)", output)
+    assert re_result is not None, "Can't find path to the test\nProgram output:\n%s\n" % (output)
+
+    test_path = re_result.group(1)
+
+    # After the path there is a program result
+    index = output.find("\n")
+    test_result = output[index + 1:]
+
+    return test_path, test_result
+
+
+def get_exp_output(dut, test_path: str):
+    cmd = f"cat {test_path}.exp"
+
+    dut.expect(PROMPT)
+    dut.sendline(cmd)
+    dut.expect(cmd + EOL)
+    dut.expect(PROMPT)
+    output = dut.before
+    # Leave prompt in the buffer
+    dut.sendline()
+
+    return output
+
+
+def harness(dut):
+    """Harness for getting test result from MicroPython test and comparing to the expected output"""
+
+    test_path, test_result = get_test(dut)
+
+    if re.match("SKIP", test_result):
+        res = TestResult(status=TestResult.SKIP)
+        dut.sendline()
+        return res
+
+    exp_output = get_exp_output(dut, test_path)
+
+    test_passed = False
+    if is_test_with_regex_output(test_path):
+        expected_output_re = create_regex(exp_output)
+        if re.match(expected_output_re, test_result):
+            test_passed = True
+    elif exp_output == test_result:
+        test_passed = True
+
+    if test_passed:
+        return TestResult(status=TestResult.OK)
+    else:
+        msg = f"Incorrect result!\n\nExpected result:\n{exp_output}\nTest result:\n{test_result}\n"
+        return TestResult(msg=msg, status=TestResult.FAIL)
---
+++ micropython/micropython_repl.py
@@ -0,0 +1,103 @@
+import re
+
+from trunner.types import TestResult
+
+PROMPT = r"(\r+)\x1b\[0J" + r"\(psh\)% "
+EOL = r"\r+\n"
+UPYTH_PROMPT = ">>>"
+MICROPYTHON = "/bin/micropython"
+
+
+def standard_regex_escape(text):
+    """In tests with regex output syntax character \\ makes next char to be interpreted as a regex character"""
+
+    result = []
+    escape = False
+    for c in text:
+        if c == "\r":
+            continue
+        if c == "\n":
+            result.append(EOL)
+            result.append("\n")
+        elif escape:
+            escape = False
+            result.append(c)
+        elif c == "\\":
+            escape = True
+        elif c in ("(", ")", "[", "]", "{", "}", ".", "*", "+", "^", "$", "?", "|"):
+            result.append("\\" + c)
+        else:
+            result.append(c)
+
+    return "".join(result)
+
+
+def create_regex(text):
+    """Tests with regex output use special syntax for expected outputs"""
+
+    text = standard_regex_escape(text)
+
+    # ######## matches zero or more lines with text
+    text = text.replace(f"########{EOL}.+", "(.|\n)*")
+    text = text.replace(f"{EOL}########", "(.|\n)*")
+    text = text.replace("########", "(.|\n)*")
+
+    return text
+
+
+def send_micropython_cmd(dut, cmd: str):
+    dut.sendline(cmd)
+    dut.expect(UPYTH_PROMPT)
+
+    return dut.before + UPYTH_PROMPT
+
+
+# TODO this test does not work
+def harness(dut):
+    """Harness for testing MicroPython REPL"""
+
+    # At first echo prints path to the test
+    dut.expect(PROMPT)
+    test_path = dut.before.rstrip()
+
+    # sending cat with path to the test to get commands to insert in micropython terminal
+    cmd = "cat " + test_path
+    dut.sendline(cmd)
+    dut.expect(cmd + EOL)
+    dut.expect(PROMPT)
+
+    script = dut.before
+    script = re.split(EOL, script)
+    script = script[:-1]  # remove empty string at the end
+
+    # sending cat with path to the expected output from test
+    dut.sendline(cmd + ".exp")
+    dut.expect(cmd + ".exp" + EOL)
+    dut.expect(PROMPT)
+
+    exp_output = dut.before
+
+    # Start micropython interpreter
+    dut.sendline(MICROPYTHON)
+    dut.expect(MICROPYTHON + EOL)
+
+    test_output = []
+
+    for line in script:
+        prompts = [">>>", "..."]
+        idx = dut.expect_exact(prompts)
+        test_output.append(dut.before + prompts[idx])
+        dut.sendline(line)
+
+    dut.sendcontrol("d")
+    dut.expect(PROMPT)
+
+    test_output.append(dut.before)
+    test_output = "".join(test_output)
+    expected_output_re = create_regex(exp_output)
+
+    if re.match(expected_output_re, test_output):
+        return TestResult(status=TestResult.OK)
+    else:
+        msg = f"Incorrect output\n\nExpected result:\n{expected_output_re}\nTest result:\n{test_output}"
+        return TestResult(msg=msg, status=TestResult.FAIL)
