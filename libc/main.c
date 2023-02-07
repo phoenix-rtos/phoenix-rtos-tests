ba79bb94d4ab422aef1888e9b535ac18536bce19
Damian Loewnau <damian.loewnau@phoenix-rtos.com>
2023-02-07 15:48:18 +0100

    libc: create tmp and etc if didn't exist
    JIRA: PD-329

--- libc/main.c
+++ libc/main.c
@@ -14,13 +14,13 @@
  */
 
 #include <stdlib.h>
+#include <string.h>
 #include "unity_fixture.h"
+#include "common.h"
 
 /* no need for forward declarations, RUN_TEST_GROUP does it by itself */
 void runner(void)
 {
-	setenv("POSIXLY_CORRECT", "y", 1);
-
 	RUN_TEST_GROUP(stdio_fopenfclose);
 	RUN_TEST_GROUP(stdio_line);
 	RUN_TEST_GROUP(stdio_getput);
@@ -39,13 +39,80 @@ void runner(void)
 	RUN_TEST_GROUP(unistd_file);
 	RUN_TEST_GROUP(wchar_wcscmp);
 	RUN_TEST_GROUP(test_pthread_cond);
+}
 
-	unsetenv("POSIXLY_CORRECT");
+
+/* crete directory unless it exists */
+static int libc_createDirIfMissing(const char *path)
+{
+	struct stat buffer;
+
+	if (stat(path, &buffer) != 0) {
+		if (errno == ENOENT) {
+			if (mkdir(path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) != 0) {
+				fprintf(stderr, "Creating %s directory by mkdir failed: %s\n", path, strerror(errno));
+				return -1;
+			}
+		}
+		else {
+			fprintf(stderr, "stat() on %s directory failed: %s\n", path, strerror(errno));
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
+
+/* crete file with optional data (may be NULL) unless it exists */
+static int libc_createFileIfMissing(const char *path, const char *charData)
+{
+	struct stat buffer;
+
+	if (stat(path, &buffer) != 0) {
+		if (errno == ENOENT) {
+			if (_create_file(path, charData) != 0) {
+				fprintf(stderr, "Creating %s file failed: %s\n", path, strerror(errno));
+				return -1;
+			}
+		}
+		else {
+			fprintf(stderr, "stat() on %s file failed: %s\n", path, strerror(errno));
+			return -1;
+		}
+	}
+
+	return 0;
 }
 
 
 int main(int argc, char *argv[])
 {
+	const char *var = "POSIXLY_CORRECT";
+
+	if (setenv(var, "y", 1) != 0) {
+		fprintf(stderr, "Setting %s environment variable failed: %s\n", var, strerror(errno));
+		return 1;
+	}
+
+	/* the following files may not be present on dummyfd targets,
+	create them to make libc tests common */
+	if (libc_createDirIfMissing("/tmp") < 0) {
+		unsetenv(var);
+		return 1;
+	}
+	if (libc_createDirIfMissing("/etc") < 0) {
+		unsetenv(var);
+		return 1;
+	}
+	if (libc_createFileIfMissing("/etc/passwd", "root:0B1ANiYi45IhxkfmUW155/GBd4IRE=:0:0:root:/:/bin/sh")) {
+		unsetenv(var);
+		return 1;
+	}
+
 	UnityMain(argc, (const char **)argv, runner);
+
+	unsetenv(var);
+
 	return 0;
 }
