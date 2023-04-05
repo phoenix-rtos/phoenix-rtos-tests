898c994d7be0bafb11161e956b768dd8826bbb35
Damian Modzelewski <damian.modzelewski@phoenix-rtos.com>
2023-04-05 12:00:39 +0200

    libc: scanf tests: add test groups for a, e, f, g specifiers
    JIRA: CI-193

--- libc/main.c
+++ libc/main.c
@@ -49,6 +49,7 @@ void runner(void)
 	RUN_TEST_GROUP(stdio_scanf_u);
 	RUN_TEST_GROUP(stdio_scanf_o);
 	RUN_TEST_GROUP(stdio_scanf_x);
+	RUN_TEST_GROUP(stdio_scanf_aefg);
 }
 
 /* crete directory unless it exists */
