8e081bac3a5d1029ade751bf1145d6e32964605f
Mateusz Bloch <mateusz.bloch@phoenix-rtos.com>
2023-06-27 12:49:01 +0200

    libc: add stdlib directory
    JIRA CI-238

---
+++ libc/stdlib/main.c
@@ -0,0 +1,33 @@
+/*
+ * Phoenix-RTOS
+ *
+ * test-libc-stdio
+ *
+ * Main entry point.
+ *
+ * Copyright 2023 Phoenix Systems
+ * Author: Mateusz Bloch
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#include "unity_fixture.h"
+
+/* no need for forward declarations, RUN_TEST_GROUP does it by itself */
+void runner(void)
+{
+	RUN_TEST_GROUP(strtod_family);
+	RUN_TEST_GROUP(stdlib_alloc);
+	RUN_TEST_GROUP(stdlib_env);
+	RUN_TEST_GROUP(stdlib_bsearch);
+}
+
+
+int main(int argc, char *argv[])
+{
+	UnityMain(argc, (const char **)argv, runner);
+
+	return 0;
+}
