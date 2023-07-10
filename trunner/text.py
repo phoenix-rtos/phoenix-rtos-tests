9a48418b238eb80d5620ba820a625c5de840b393
Mateusz Bloch <mateusz.bloch@phoenix-rtos.com>
2023-07-10 20:00:44 +0200

    trunner: enable colorful logs
    JIRA CI-296

--- trunner/text.py
+++ trunner/text.py
@@ -1,37 +1,21 @@
-# from colorama import Style, Fore
-
-# def bold(s: str) -> str:
-#    return Style.BRIGHT + s + Style.RESET_ALL
-#
-# def red(s: str) -> str:
-#    return Fore.RED + s + Fore.RESET
-#
-# def green(s: str) -> str:
-#    return Fore.GREEN + s + Fore.RESET
-#
-# def yellow(s: str) -> str:
-#    return Fore.YELLOW + s + Fore.RESET
-#
-#
-# def blue(s: str) -> str:
-#    return Fore.BLUE + s + Fore.RESET
+from colorama import Style, Fore
 
 
 def bold(s: str) -> str:
-    return s
+    return Style.BRIGHT + s + Style.RESET_ALL
 
 
 def red(s: str) -> str:
-    return s
+    return Fore.RED + s + Fore.RESET
 
 
 def green(s: str) -> str:
-    return s
+    return Fore.GREEN + s + Fore.RESET
 
 
 def yellow(s: str) -> str:
-    return s
+    return Fore.YELLOW + s + Fore.RESET
 
 
 def blue(s: str) -> str:
-    return s
+    return Fore.BLUE + s + Fore.RESET
