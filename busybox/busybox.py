771b93d128ae3738fec70830e11b62f21654b011
Mateusz Bloch <mateusz.bloch@phoenix-rtos.com>
2023-10-13 15:48:18 +0200

    busybox: fix recording of test durations to xml file

--- busybox/busybox.py
+++ busybox/busybox.py
@@ -3,45 +3,51 @@
 #
 # The harness for the Busybox Test Suite
 #
-# Copyright 2022 Phoenix Systems
-# Authors: Damian Loewnau
+# Copyright 2022, 2023 Phoenix Systems
+# Authors: Damian Loewnau, Mateusz Bloch
 #
+import pexpect
 
 from trunner.ctx import TestContext
 from trunner.dut import Dut
-from trunner.types import TestResult, TestSubResult, Status
+from trunner.types import TestResult, Status
 
 
 def harness(dut: Dut, ctx: TestContext, result: TestResult):
-    subresult = None
-    msg = []
+    msg = ""
     test_status = Status.OK
 
     RESULT = r"(?P<status>PASS|SKIPPED|FAIL): (?P<name>.+?)\r+\n"
     FINAL = r"\*\*\*\*(The Busybox Test Suite completed|A single test of the Busybox Test Suite completed)\*\*\*\*\r+\n"
-    MESSAGE = r"(?P<line>.*?)\r+\n"
+    MESSAGE = r"^[+-](?P<line>.*?)\r+\n"
 
     while True:
         idx = dut.expect([RESULT, FINAL, MESSAGE], timeout=90)
         parsed = dut.match.groupdict()
 
         if idx == 2:
-            msg.append("\t\t" + parsed["line"])
-            continue
-
-        if subresult:
-            # We ended processing test result and message
-            if msg and subresult.status == Status.FAIL:
-                test_status = Status.FAIL
-                subresult.msg = "\n".join(msg)
-                msg = []
-
-            result.add_subresult_obj(subresult)
-            subresult = None
+            # BusyBox tests, a message can appear either before or after the RESULT.
+            msg += "-" + parsed["line"] + "\n\r\t\t"
 
         if idx == 0:
-            subresult = TestSubResult(subname=parsed["name"], status=Status.from_str(parsed["status"]))
-        elif idx == 1:
+            status = Status.from_str(parsed["status"])
+
+            if status == Status.FAIL:
+                test_status = status
+                # Capture fail message after RESULT if any exists
+                while True:
+                    try:
+                        dut.expect([MESSAGE], timeout=1)
+                        msg += "+" + dut.match.groupdict()["line"] + "\n\r\t\t"
+
+                    except pexpect.TIMEOUT:
+                        msg = msg[:-4]
+                        break
+
+            result.add_subresult(subname=parsed["name"], status=status, msg=msg)
+            msg = ""
+
+        if idx == 1:
             break
 
     return TestResult(status=test_status)
