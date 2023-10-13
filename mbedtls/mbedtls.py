f8e882b0a372ce9d49f3e14098a5b6474350753e
Mateusz Bloch <mateusz.bloch@phoenix-rtos.com>
2023-10-13 15:19:09 +0200

    mbedtls: fix recording of test durations to xml file

--- mbedtls/mbedtls.py
+++ mbedtls/mbedtls.py
@@ -1,36 +1,22 @@
+import pexpect
 import re
 
 from trunner.ctx import TestContext
 from trunner.dut import Dut
-from trunner.types import TestResult, Status, TestSubResult
+from trunner.types import TestResult, Status
 
 
 def harness(dut: Dut, ctx: TestContext, result: TestResult):
-    subresult = None
-    msg = []
     test_status = Status.OK
+    msg = ""
 
     RESULT = r"(?P<name>.{67}\s)(?P<status>(PASS|----|FAILED))\r+\n"
     MSG_LINE = r"  (?P<line>[^\r\n]+?)\r+\n"
     FINAL = r"(?P<status>FAILED|PASSED)\s\((?P<nr>\d+)\s/\s(?P<total_nr>\d+)\stests\s\(\d+\sskipped\)\)"
 
     while True:
-        idx = dut.expect([RESULT, FINAL, MSG_LINE], timeout=90)
+        idx = dut.expect([RESULT, FINAL], timeout=90)
         parsed = dut.match.groupdict()
-
-        if idx == 2:
-            msg.append(parsed["line"])
-            continue
-
-        if subresult:
-            # We ended processing test result and message
-            if msg and subresult.status == Status.FAIL:
-                test_status = Status.FAIL
-                subresult.msg = " ".join(msg)
-                msg = []
-
-            result.add_subresult_obj(subresult)
-            subresult = None
 
         if idx == 0:
             if parsed["status"] == "----":
@@ -38,9 +24,23 @@ def harness(dut: Dut, ctx: TestContext, result: TestResult):
             else:
                 status = Status.from_str(parsed["status"])
 
+            if status == Status.FAIL:
+                test_status = status
+                # Capture fail message if any exists
+                while True:
+                    try:
+                        dut.expect([MSG_LINE], timeout=1)
+                        msg += dut.match.groupdict()["line"] + "\n\r\t\t"
+
+                    except pexpect.TIMEOUT:
+                        msg = msg[:-4]
+                        break
+
             # if there are dots after test name - remove them
             subname = re.sub(r" \.+ ", "", parsed["name"])
-            subresult = TestSubResult(subname=subname, status=status)
+            result.add_subresult(subname=subname, status=status, msg=msg)
+            msg = ""
+
         if idx == 1:
             break
 
