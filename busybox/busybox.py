287bc4fb629eb9018993b6c16dc5e68783f7c516
Mateusz Bloch <mateusz.bloch@phoenix-rtos.com>
2023-10-26 13:46:04 +0200

    busybox: fix measuring test duration
    The subtest result was incorrectly finished (added to result) one line
    after the result (so actually computed the time of the next test).
    
    JIRA: CI-384

--- busybox/busybox.py
+++ busybox/busybox.py
@@ -9,7 +9,25 @@
 
 from trunner.ctx import TestContext
 from trunner.dut import Dut
-from trunner.types import TestResult, TestSubResult, Status
+from trunner.types import TestResult, Status
+
+
+EXAMPLE_INPUT = """
+---Testcase 'busybox as unknown name' starting---
+echo -ne '' >input
+echo -ne '' | ./unknown 2>&1
+SKIPPED: busybox --help
+PASS: busybox as unknown name
+---Testcase 'date-@-works' starting---
+FAIL: date-@-works
++ TZ=EET-2EEST,M3.5.0/3,M10.5.0/4 busybox date -d @1288486799
++ test xSun Oct 31 03:59:59 EEST 2010 = xSun Oct 31 00:59:59  2010
+---Testcase 'date-R-works' starting---
+PASS: date-R-works
+---Testcase 'bunzip2: doesnt exist' starting---
+- t_actual differ: char 13, line 1
+FAIL: bunzip2: doesnt exist
+"""
 
 
 def harness(dut: Dut, ctx: TestContext, result: TestResult):
@@ -17,31 +35,39 @@ def harness(dut: Dut, ctx: TestContext, result: TestResult):
     msg = []
     test_status = Status.OK
 
+    START = r"---Testcase '(?P<name>.+?)' starting---\r+\n"
     RESULT = r"(?P<status>PASS|SKIPPED|FAIL): (?P<name>.+?)\r+\n"
     FINAL = r"\*\*\*\*(The Busybox Test Suite completed|A single test of the Busybox Test Suite completed)\*\*\*\*\r+\n"
     MESSAGE = r"(?P<line>.*?)\r+\n"
 
     while True:
-        idx = dut.expect([RESULT, FINAL, MESSAGE], timeout=90)
+        idx = dut.expect([START, RESULT, FINAL, MESSAGE], timeout=90)
         parsed = dut.match.groupdict()
 
-        if idx == 2:
-            msg.append("\t\t" + parsed["line"])
+        if idx == 0:
+            subresult = None
             continue
 
-        if subresult:
-            # We ended processing test result and message
-            if msg and subresult.status == Status.FAIL:
+        if idx == 3:
+            # "\n\t\t" used to create more readable multiline message when printed
+            line_msg = "\n\t\t" + parsed["line"]
+            # append extra message to the last test (if available)
+            if subresult and subresult.status == Status.FAIL:
+                subresult.msg += line_msg
+            else:
+                # message may also appear during the test
+                msg.append(line_msg)
+
+        if idx == 1:
+            status = Status.from_str(parsed["status"])
+
+            if status == Status.FAIL:
                 test_status = Status.FAIL
-                subresult.msg = "\n".join(msg)
-                msg = []
 
-            result.add_subresult_obj(subresult)
-            subresult = None
+            subresult = result.add_subresult(subname=parsed["name"], status=status, msg="".join(msg))
+            msg.clear()
 
-        if idx == 0:
-            subresult = TestSubResult(subname=parsed["name"], status=Status.from_str(parsed["status"]))
-        elif idx == 1:
+        elif idx == 2:
             break
 
     return TestResult(status=test_status)
