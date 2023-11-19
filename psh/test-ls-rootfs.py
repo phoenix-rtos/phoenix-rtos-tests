39050601efe1ae5b43f89c672dec2b6d03406236
Gerard Swiderski <gerard@swiderski.eu>
2023-11-19 21:15:29 +0100

    psh: ls-rootfs: add `export`, `unset` internal commands
    Adds 'export' and 'unset' commands to pshapp internal commands.
    These commands are used internally and in psh scripts (rc.psh)
    to manage environment variables.
    
    JIRA: RTOS-685 RTOS-682

--- psh/test-ls-rootfs.py
+++ psh/test-ls-rootfs.py
@@ -21,8 +21,8 @@ ROOT_TEST_DIR = 'test_ls_rootfs_dir'
 
 
 def assert_ls_pshcmds(p, psh_cmds):
-    # history, exit, clear and reset symlinks shouldn't be present in bin
-    psh_cmds = set(psh_cmds) - {'history', 'exit', 'clear', 'reset'}
+    # history, exit, clear, reset, export and unset symlinks shouldn't be present in bin
+    psh_cmds = set(psh_cmds) - {'history', 'exit', 'clear', 'reset', 'export', 'unset'}
 
     psh_cmd_pattern = CONTROL_CODE
     psh_cmd_pattern += r'(?P<cmd>' + '|'.join(psh_cmds) + r')'
