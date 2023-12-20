2a3457779e3115f1300dfb0bb71e8035d87267a9
lukileczo <lukasz.leczkowski@phoenix-rtos.com>
2023-12-20 17:51:53 +0100

    devices/grlib-multi: add tests for SpaceWire
    JIRA: RTOS-709

--- devices/grlib-multi/test-grlib-multi.c
+++ devices/grlib-multi/test-grlib-multi.c
@@ -64,6 +64,15 @@
 #define TEST_SPI_SLOW_CLK 0
 #define TEST_SPI_FAST_CLK 1
 
+#define TEST_SPW_ID0 id_spw0
+#define TEST_SPW_ID1 id_spw1
+
+#define TEST_SPW_PATH0 "/dev/spw0"
+#define TEST_SPW_PATH1 "/dev/spw1"
+
+#define TEST_SPW_ADDR0 0x1
+#define TEST_SPW_ADDR1 0x2
+
 
 static struct {
 	uint8_t txBuff[TEST_SPI_BUFFSZ_LARGE];
@@ -501,6 +510,200 @@ TEST(test_adc, adcDefaultConfigConversion)
 }
 
 
+/* SpaceWire tests - assume physical loopback SPW0-SPW1 */
+
+
+static unsigned int test_spwConfigureRx(const oid_t rxOid, const size_t nPackets)
+{
+	/* Configure RX on SPW0 */
+	msg_t msg = {
+		.type = mtDevCtl,
+		.i = { .data = NULL, .size = 0 },
+		.o = { .data = NULL, .size = 0 },
+	};
+
+	multi_i_t *idevctl = (multi_i_t *)msg.i.raw;
+	multi_o_t *odevctl = (multi_o_t *)msg.o.raw;
+
+	idevctl->id = TEST_SPW_ID0;
+	idevctl->spw.type = spw_rxConfig;
+	idevctl->spw.task.rxConfig.nPackets = nPackets;
+
+	TEST_ASSERT_EQUAL_INT(0, msgSend(rxOid.port, &msg));
+	TEST_ASSERT_EQUAL_INT(nPackets, odevctl->err);
+
+	return odevctl->val;
+}
+
+
+static void test_spwTx(const oid_t txOid, uint8_t *txBuf, const size_t txBufsz, const size_t nPackets, bool async)
+{
+	msg_t msg = {
+		.type = mtDevCtl,
+		.i = { .data = txBuf, .size = txBufsz },
+		.o = { .data = NULL, .size = 0 },
+	};
+
+	multi_i_t *idevctl = (multi_i_t *)msg.i.raw;
+	multi_o_t *odevctl = (multi_o_t *)msg.o.raw;
+
+	idevctl->id = TEST_SPW_ID1;
+	idevctl->spw.type = spw_tx;
+	idevctl->spw.task.tx.nPackets = nPackets;
+	idevctl->spw.task.tx.async = async;
+
+	TEST_ASSERT_EQUAL_INT(0, msgSend(txOid.port, &msg));
+	TEST_ASSERT_EQUAL_INT(nPackets, odevctl->err);
+}
+
+
+static void test_spwRxRead(const oid_t rxOid, const unsigned int firstDesc, uint8_t *rxBuf, size_t rxBufsz, spw_rxPacket_t *packets, const size_t nPackets)
+{
+	msg_t msg = {
+		.type = mtDevCtl,
+		.i = { .data = NULL, .size = 0 },
+		.o = { .data = rxBuf, .size = rxBufsz },
+	};
+	multi_i_t *idevctl = (multi_i_t *)msg.i.raw;
+	multi_o_t *odevctl = (multi_o_t *)msg.o.raw;
+
+	idevctl->id = TEST_SPW_ID0;
+	idevctl->spw.type = spw_rx;
+	idevctl->spw.task.rx.nPackets = nPackets;
+	idevctl->spw.task.rx.firstDesc = firstDesc;
+
+	TEST_ASSERT_EQUAL_INT(0, msgSend(rxOid.port, &msg));
+	TEST_ASSERT_EQUAL_INT(nPackets, odevctl->err);
+
+	for (size_t i = 0; i < nPackets; i++) {
+		rxBuf += multi_spwDeserializeRxMsg(rxBuf, &packets[i]);
+	}
+}
+
+
+static void test_spwRxTx(const size_t nPackets, bool async)
+{
+	oid_t rxOid = test_getOid(TEST_SPW_PATH0);
+	unsigned int firstDesc = test_spwConfigureRx(rxOid, nPackets);
+
+	oid_t txOid = test_getOid(TEST_SPW_PATH1);
+	static const uint8_t hdr[] = { TEST_SPW_ADDR0, /* protocol ID */ 0x5 }, data[] = { 0x1, 0x2, 0x3, 0x4 };
+	static const size_t hdrSz = sizeof(hdr), dataSz = sizeof(data);
+	const size_t txBufsz = (SPW_TX_MIN_BUFSZ + hdrSz + dataSz) * nPackets;
+	uint8_t *txBuf = malloc(txBufsz);
+	TEST_ASSERT_NOT_NULL(txBuf);
+
+	size_t size = 0;
+	for (size_t i = 0; i < nPackets; i++) {
+		size_t ret = multi_spwSerializeTxMsg(SPW_TX_FLG_HDR_LEN(hdrSz), dataSz, hdr, data, txBuf + size, txBufsz - size);
+		TEST_ASSERT_NOT_EQUAL(0, ret);
+		size += ret;
+	}
+	test_spwTx(txOid, txBuf, size, nPackets, async);
+
+	/* Receive packet */
+	const size_t rxBufsz = (SPW_RX_MIN_BUFSZ + hdrSz + dataSz) * nPackets;
+	uint8_t *rxBuf = malloc(rxBufsz);
+	TEST_ASSERT_NOT_NULL(rxBuf);
+
+	spw_rxPacket_t packets[nPackets];
+	test_spwRxRead(rxOid, firstDesc, rxBuf, rxBufsz, packets, nPackets);
+
+	for (size_t i = 0; i < nPackets; i++) {
+		TEST_ASSERT_EQUAL(hdrSz + dataSz, packets[i].flags & SPW_RX_LEN_MSK);
+		TEST_ASSERT_EQUAL_HEX8_ARRAY(hdr, packets[i].buf, hdrSz);
+		TEST_ASSERT_EQUAL_HEX8_ARRAY(data, packets[i].buf + hdrSz, dataSz);
+	}
+
+	free(txBuf);
+	free(rxBuf);
+}
+
+
+TEST_GROUP(test_spw);
+
+
+TEST_SETUP(test_spw)
+{
+}
+
+
+TEST_TEAR_DOWN(test_spw)
+{
+}
+
+
+TEST(test_spw, spwSetAddress)
+{
+	oid_t oid = test_getOid(TEST_SPW_PATH0);
+	msg_t msg = {
+		.type = mtDevCtl,
+		.i = { .data = NULL, .size = 0 },
+		.o = { .data = NULL, .size = 0 },
+	};
+	multi_i_t *idevctl = (multi_i_t *)msg.i.raw;
+	multi_o_t *odevctl = (multi_o_t *)msg.o.raw;
+
+	idevctl->id = TEST_SPW_ID0;
+	idevctl->spw.type = spw_config;
+	idevctl->spw.task.config.node.addr = TEST_SPW_ADDR0;
+	idevctl->spw.task.config.node.mask = 0x0;
+	idevctl->spw.task.config.dma.addr = TEST_SPW_ADDR0;
+	idevctl->spw.task.config.dma.mask = 0x0;
+
+	TEST_ASSERT_EQUAL_INT(0, msgSend(oid.port, &msg));
+	TEST_ASSERT_EQUAL_INT(0, odevctl->err);
+
+	oid = test_getOid(TEST_SPW_PATH1);
+	idevctl->id = TEST_SPW_ID1;
+	idevctl->spw.type = spw_config;
+	idevctl->spw.task.config.node.addr = TEST_SPW_ADDR1;
+	idevctl->spw.task.config.node.mask = 0x0;
+	idevctl->spw.task.config.dma.addr = TEST_SPW_ADDR1;
+	idevctl->spw.task.config.dma.mask = 0x0;
+
+	TEST_ASSERT_EQUAL_INT(0, msgSend(oid.port, &msg));
+	TEST_ASSERT_EQUAL_INT(0, odevctl->err);
+}
+
+
+TEST(test_spw, spwTxRxSinglePacketSync)
+{
+	test_spwRxTx(1, false);
+}
+
+
+TEST(test_spw, spwTxRxSinglePacketAsync)
+{
+	test_spwRxTx(1, true);
+}
+
+
+TEST(test_spw, spwTxRxMultiplePacketsSync)
+{
+	test_spwRxTx(10, false);
+}
+
+
+TEST(test_spw, spwTxRxMultiplePacketsAsync)
+{
+	test_spwRxTx(10, true);
+}
+
+TEST(test_spw, spwTxRxBigNumberOfPacketsSync)
+{
+	/* Sync TX number of packets must not be bigger than SPW_TX_DESC_CNT */
+	test_spwRxTx(64, false);
+}
+
+
+TEST(test_spw, spwTxRxBigNumberOfPacketsAsync)
+{
+	/* Async TX number of packets must not be bigger than SPW_RX_DESC_CNT */
+	test_spwRxTx(128, true);
+}
+
+
 /* Test runner */
 
 
@@ -539,6 +742,18 @@ TEST_GROUP_RUNNER(test_adc)
 }
 
 
+TEST_GROUP_RUNNER(test_spw)
+{
+	RUN_TEST_CASE(test_spw, spwSetAddress);
+	RUN_TEST_CASE(test_spw, spwTxRxSinglePacketSync);
+	RUN_TEST_CASE(test_spw, spwTxRxSinglePacketAsync);
+	RUN_TEST_CASE(test_spw, spwTxRxMultiplePacketsSync);
+	RUN_TEST_CASE(test_spw, spwTxRxMultiplePacketsAsync);
+	RUN_TEST_CASE(test_spw, spwTxRxBigNumberOfPacketsSync);
+	RUN_TEST_CASE(test_spw, spwTxRxBigNumberOfPacketsAsync);
+}
+
+
 void runner(void)
 {
 	/* GPIO tests were tailored to be run on GR716-MINI
@@ -552,6 +767,9 @@ void runner(void)
 #endif
 #if ADC_CNT > 0
 	RUN_TEST_GROUP(test_adc);
+#endif
+#if SPW_CNT > 1
+	RUN_TEST_GROUP(test_spw);
 #endif
 }
 
