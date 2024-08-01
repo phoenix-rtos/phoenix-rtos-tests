c8d5a16e9d6c36bd58a046434c00c2dbe5e9741b
Łukasz Leczkowski <lukasz.leczkowski@phoenix-rtos.com>
2024-08-01 15:35:01 +0200

    rename sparcv8leon3 to sparcv8leon
    JIRA: RTOS-883

--- devices/grlib-multi/test-grlib-multi.c
+++ devices/grlib-multi/test-grlib-multi.c
@@ -21,9 +21,9 @@
 #include <sys/types.h>
 #include <sys/platform.h>
 #if defined(__CPU_GR716)
-#include <phoenix/arch/sparcv8leon3/gr716/gr716.h>
+#include <phoenix/arch/sparcv8leon/gr716/gr716.h>
 #elif defined(__CPU_GR712RC)
-#include <phoenix/arch/sparcv8leon3/gr712rc/gr712rc.h>
+#include <phoenix/arch/sparcv8leon/gr712rc/gr712rc.h>
 #else
 #error "Unsupported target"
 #endif
