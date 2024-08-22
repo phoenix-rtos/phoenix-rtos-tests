ae158746c54bc219b508af8f949b946379afc279
ziemowit.leszczynski <ziemowit.leszczynski@phoenix-rtos.com>
2024-08-22 13:50:44 +0200

    libc: add fread and fwrite tests
    JIRA: RTOS-892

--- libc/stdio/main.c
+++ libc/stdio/main.c
@@ -27,6 +27,8 @@ void runner(void)
 	RUN_TEST_GROUP(stdio_fileseek);
 	RUN_TEST_GROUP(stdio_fileop);
 	RUN_TEST_GROUP(stdio_bufs);
+	RUN_TEST_GROUP(stdio_fread);
+	RUN_TEST_GROUP(stdio_fwrite);
 }
 
 
