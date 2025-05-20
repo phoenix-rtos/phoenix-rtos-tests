9a4877ebd72adcbe0630ac3841b87131cfa28455
Marek Białowąs <marek.bialowas@phoenix-rtos.com>
2025-05-20 16:45:01 +0200

    libc: add signal sigmask, sigsuspend tests
    Tests showcasing issues:
    https://github.com/phoenix-rtos/phoenix-rtos-project/issues/1234
    https://github.com/phoenix-rtos/phoenix-rtos-project/issues/1306
    
    JIRA: RTOS-1059

---
+++ libc/signal/main.c
@@ -0,0 +1,33 @@
+/*
+ * Phoenix-RTOS
+ *
+ * test-libc-signal
+ *
+ * Main entry point.
+ *
+ * Copyright 2025 Phoenix Systems
+ * Author: Marek Bialowas
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#include <stdlib.h>
+
+#include <unity_fixture.h>
+
+
+/* no need for forward declarations, RUN_TEST_GROUP does it by itself */
+void runner(void)
+{
+	RUN_TEST_GROUP(mask);
+	RUN_TEST_GROUP(handler);
+	RUN_TEST_GROUP(sigsuspend);
+}
+
+
+int main(int argc, char *argv[])
+{
+	return (UnityMain(argc, (const char **)argv, runner) == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
