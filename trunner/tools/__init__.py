2cc06db40d9b1f2fc1cfba6d1140ecdb2290b32f
Mateusz Bloch <mateusz.bloch@phoenix-rtos.com>
2025-05-27 20:00:38 +0200

    trunner: fix flash armv7m4
    * fix correct handle of the flash process
    * provide correct path to the image
    * add detection of flash status via exit code
    
    problems started with this commit:
    * trunner: implement reboot by debugger for armv7a9 & armv7m4 (1c676a4)
    
    JIRA: CI-545

--- trunner/tools/__init__.py
+++ trunner/tools/__init__.py
@@ -1,11 +1,12 @@
 from .phoenix import Phoenixd, PhoenixdError, Psu, PsuError, wait_for_vid_pid
-from .gdb import GdbInteractive, OpenocdGdbServer, OpenocdProcess, JLinkGdbServer
+from .gdb import GdbInteractive, OpenocdGdbServer, OpenocdProcess, JLinkGdbServer, OpenocdError
 
 __all__ = [
     "JLinkGdbServer",
     "GdbInteractive",
     "OpenocdGdbServer",
     "OpenocdProcess",
+    "OpenocdError",
     "Phoenixd",
     "PhoenixdError",
     "Psu",
