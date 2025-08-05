7ce49b2d5373f84dee85aef5f91a7e90a142edff
Jakub Klimek <jakub.klimek@phoenix-rtos.com>
2025-08-05 16:37:51 +0200

    libc: more signal tests
    Additional signal tests for phoenix-rtos/phoenix-rtos-kernel#670 moving signal handling to kernel
    
    JIRA: RTOS-736

--- libc/signal/handler.c
+++ libc/signal/handler.c
@@ -18,6 +18,8 @@
 #include <signal.h>
 #include <unistd.h>
 #include <errno.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
 #include <unity_fixture.h>
 
@@ -34,24 +36,45 @@ TEST_TEAR_DOWN(handler)
 {
 	/* unblock all signals */
 	sigset_t set;
-	sigemptyset(&set);
-	sigprocmask(SIG_SETMASK, &set, NULL);
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&set));
+	TEST_ASSERT_EQUAL_INT(0, sigprocmask(SIG_SETMASK, &set, NULL));
 
 	/* disable any active alarm timer */
 	alarm(0);
 
 	/* set default signal disposition for all signals */
 	for (int signo = 1; signo < USERSPACE_NSIG; ++signo) {
-		signal(signo, SIG_DFL);
+		/* SIGKILL/SIGSTOP dispositions can't be changed */
+		if (signal_is_unblockable(signo)) {
+			continue;
+		}
+		TEST_ASSERT_NOT_EQUAL(SIG_ERR, signal(signo, SIG_DFL));
 	}
 }
 
 
 static volatile sig_atomic_t handler_haveSignal;
 static volatile sigset_t handler_sigset;
+static volatile sig_atomic_t handler_countdown;
+static volatile struct sigaction handler_sigaction;
 
 
-void sighandler(int sig)
+static pid_t safe_fork(void)
+{
+	pid_t pid;
+	if ((pid = fork()) < 0) {
+		if (errno == ENOSYS) {
+			TEST_IGNORE_MESSAGE("fork syscall not supported");
+		}
+		else {
+			FAIL("fork");
+		}
+	}
+	return pid;
+}
+
+
+static void sighandler(int sig)
 {
 	sigset_t set;
 	sigprocmask(SIG_SETMASK, NULL, &set);
@@ -60,6 +83,41 @@ void sighandler(int sig)
 	handler_sigset = set;
 
 	handler_haveSignal |= (1u << sig);
+}
+
+
+static void sighandlerRecursive(int sig)
+{
+	if (handler_countdown > 0) {
+		handler_countdown--;
+		sighandlerRecursive(sig);
+	}
+}
+
+
+static void sighandlerReraise(int sig)
+{
+	sigset_t set;
+	sigprocmask(SIG_SETMASK, NULL, &set);
+
+	/* WARN: the write might not be atomic */
+	handler_sigset = set;
+
+	if (handler_countdown > 0) {
+		handler_countdown--;
+		raise(sig);
+	}
+}
+
+
+static void sighandlerAction(int sig)
+{
+	struct sigaction sa;
+	sa = handler_sigaction;
+	sigaction(sig, &sa, NULL);
+	if (sig == SIGUSR1) {
+		raise(sig);
+	}
 }
 
 
@@ -160,8 +218,8 @@ TEST(handler, unblock_pending_signal)
 	TEST_ASSERT_EQUAL_INT(0, sigdelset(&set, SIGALRM));
 	TEST_ASSERT_EQUAL_INT(0, sigprocmask(SIG_SETMASK, &set, NULL));
 
-	signal(SIGALRM, sighandler);
-	signal(SIGUSR1, sighandler);
+	TEST_ASSERT_NOT_EQUAL(SIG_ERR, signal(SIGALRM, sighandler));
+	TEST_ASSERT_NOT_EQUAL(SIG_ERR, signal(SIGUSR1, sighandler));
 
 	/* send signal and wait 1s to be sure it won't arrive */
 	TEST_ASSERT_EQUAL_INT(0, alarm(1));
@@ -211,7 +269,7 @@ TEST_SETUP(sigsuspend)
 		.sa_handler = sighandler,
 	};
 
-	sigemptyset(&sa.sa_mask);
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&sa.sa_mask));
 	TEST_ASSERT_EQUAL_INT(0, sigaddset(&sa.sa_mask, SIGPIPE));
 
 	TEST_ASSERT_EQUAL_INT(0, sigaction(SIGALRM, &sa, NULL));
@@ -224,13 +282,13 @@ TEST_TEAR_DOWN(sigsuspend)
 {
 	/* unblock all signals */
 	sigset_t set;
-	sigemptyset(&set);
-	sigprocmask(SIG_SETMASK, &set, NULL);
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&set));
+	TEST_ASSERT_EQUAL_INT(0, sigprocmask(SIG_SETMASK, &set, NULL));
 
 	/* set default signal disposition for all signals used is sigsuspend tests (@see TEST_SETUP(sigsuspend)) */
-	signal(SIGALRM, SIG_DFL);
-	signal(SIGUSR1, SIG_DFL);
-	signal(SIGUSR2, SIG_DFL);
+	TEST_ASSERT_NOT_EQUAL(SIG_ERR, signal(SIGALRM, SIG_DFL));
+	TEST_ASSERT_NOT_EQUAL(SIG_ERR, signal(SIGUSR1, SIG_DFL));
+	TEST_ASSERT_NOT_EQUAL(SIG_ERR, signal(SIGUSR2, SIG_DFL));
 }
 
 
@@ -250,7 +308,7 @@ TEST(sigsuspend, signal_before_handler)
 	TEST_ASSERT_EQUAL_HEX32(0, handler_haveSignal); /* signals are blocked, signal handler should not be called */
 
 	errno = 0;
-	sigsuspend(&all_unblocked);
+	TEST_ASSERT_EQUAL_INT(-1, sigsuspend(&all_unblocked));
 	TEST_ASSERT_EQUAL_INT(EINTR, errno);
 
 	/* SIGUSR1 should be handled now */
@@ -293,7 +351,7 @@ TEST(sigsuspend, signal_before_two_signals)
 	TEST_ASSERT_EQUAL_HEX32(0, handler_haveSignal); /* signals are blocked, signal handler should not be called */
 
 	errno = 0;
-	sigsuspend(&all_unblocked);
+	TEST_ASSERT_EQUAL_INT(-1, sigsuspend(&all_unblocked));
 	TEST_ASSERT_EQUAL_INT(EINTR, errno);
 
 	/* SIGUSR1 and SIGUSR2 should be handled now */
@@ -323,11 +381,11 @@ TEST(sigsuspend, signal_after)
 	TEST_ASSERT_EQUAL_INT(0, sigprocmask(SIG_SETMASK, &all_blocked, NULL));
 
 	handler_haveSignal = 0u;
-	alarm(1);
+	TEST_ASSERT_EQUAL_INT(0, alarm(1));
 	TEST_ASSERT_EQUAL_HEX32(0, handler_haveSignal); /* signals are blocked, signal handler should not be called */
 
 	errno = 0;
-	sigsuspend(&all_unblocked);
+	TEST_ASSERT_EQUAL_INT(-1, sigsuspend(&all_unblocked));
 	TEST_ASSERT_EQUAL_INT(EINTR, errno);
 
 	/* SIGALRM should be handled now */
@@ -351,4 +409,382 @@ TEST_GROUP_RUNNER(sigsuspend)
 	RUN_TEST_CASE(sigsuspend, signal_after);
 	RUN_TEST_CASE(sigsuspend, signal_before_handler);
 	RUN_TEST_CASE(sigsuspend, signal_before_two_signals);
+}
+
+
+TEST_GROUP(sigaction);
+
+
+TEST_SETUP(sigaction)
+{
+	handler_haveSignal = 0u;
+	handler_countdown = 5;
+}
+
+
+TEST_TEAR_DOWN(sigaction)
+{
+	/* unblock all signals */
+	sigset_t set;
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&set));
+	TEST_ASSERT_EQUAL_INT(0, sigprocmask(SIG_SETMASK, &set, NULL));
+
+	/* disable any active alarm timer */
+	alarm(0);
+
+	/* set default signal disposition for all signals */
+	for (int signo = 1; signo < USERSPACE_NSIG; ++signo) {
+		/* SIGKILL/SIGSTOP dispositions can't be changed */
+		if (signal_is_unblockable(signo)) {
+			continue;
+		}
+		TEST_ASSERT_NOT_EQUAL(SIG_ERR, signal(signo, SIG_DFL));
+	}
+}
+
+
+TEST(sigaction, signal_termination_statuscode)
+{
+	static const int termination_signals[] = {
+		SIGILL, SIGSEGV, SIGHUP, SIGINT, SIGQUIT, SIGTRAP, SIGABRT, SIGFPE, SIGKILL, SIGBUS,
+		SIGSYS, SIGPIPE, SIGALRM, SIGTERM, SIGIO, SIGXCPU, SIGXFSZ, SIGVTALRM, SIGPROF, SIGUSR1, SIGUSR2
+	};
+
+	for (int i = 0; i < sizeof(termination_signals) / sizeof(termination_signals[0]); ++i) {
+		int sig = termination_signals[i];
+		pid_t pid = safe_fork();
+		TEST_ASSERT_GREATER_OR_EQUAL(0, pid);
+		if (pid > 0) {
+			int code;
+			TEST_ASSERT_EQUAL_INT(pid, waitpid(pid, &code, 0));
+			TEST_ASSERT_EQUAL_INT(true, WIFSIGNALED(code));
+			TEST_ASSERT_EQUAL_HEX32(sig, WTERMSIG(code));
+		}
+		else {
+			signal(sig, SIG_DFL);
+			raise(sig);
+			_exit(0);
+		}
+	}
+}
+
+
+TEST(sigaction, signal_default_ignored)
+{
+	static const int ignored_signals[] = { SIGURG, SIGCHLD, SIGWINCH };
+
+	for (int i = 0; i < sizeof(ignored_signals) / sizeof(ignored_signals[0]); ++i) {
+		int sig = ignored_signals[i];
+		pid_t pid = safe_fork();
+		TEST_ASSERT_GREATER_OR_EQUAL(0, pid);
+		if (pid > 0) {
+			int code;
+			TEST_ASSERT_EQUAL_INT(pid, waitpid(pid, &code, 0));
+			TEST_ASSERT_EQUAL_INT(true, WIFEXITED(code));
+			TEST_ASSERT_EQUAL_HEX32(0, WEXITSTATUS(code));
+		}
+		else {
+			signal(sig, SIG_DFL);
+			raise(sig);
+			_exit(0);
+		}
+	}
+}
+
+
+/* check if signal action is performed on unmasking after being changed */
+TEST(sigaction, unmask_changed_action_handler_to_ignore)
+{
+	sigset_t masked, empty;
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&empty));
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&masked));
+	TEST_ASSERT_EQUAL_INT(0, sigaddset(&masked, SIGUSR1));
+
+	TEST_ASSERT_EQUAL_INT(0, sigprocmask(SIG_SETMASK, &masked, NULL));
+	TEST_ASSERT_NOT_EQUAL(SIG_ERR, signal(SIGUSR1, sighandler));
+	TEST_ASSERT_EQUAL_INT(0, raise(SIGUSR1));
+	TEST_ASSERT_NOT_EQUAL(SIG_ERR, signal(SIGUSR1, SIG_IGN));
+	TEST_ASSERT_EQUAL_INT(0, sigprocmask(SIG_SETMASK, &empty, NULL));
+	TEST_ASSERT_EQUAL_HEX32(0, handler_haveSignal);
+	TEST_ASSERT_NOT_EQUAL(SIG_ERR, signal(SIGUSR1, sighandler));
+	TEST_ASSERT_EQUAL_HEX32(0, handler_haveSignal);
+}
+
+
+TEST(sigaction, unmask_changed_action_default_to_ignore)
+{
+	sigset_t masked, empty;
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&empty));
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&masked));
+	TEST_ASSERT_EQUAL_INT(0, sigaddset(&masked, SIGUSR1));
+
+	TEST_ASSERT_EQUAL_INT(0, sigprocmask(SIG_SETMASK, &masked, NULL));
+	TEST_ASSERT_NOT_EQUAL(SIG_ERR, signal(SIGUSR1, SIG_DFL));
+	TEST_ASSERT_EQUAL_INT(0, raise(SIGUSR1));
+	TEST_ASSERT_NOT_EQUAL(SIG_ERR, signal(SIGUSR1, SIG_IGN));
+	TEST_ASSERT_EQUAL_INT(0, sigprocmask(SIG_SETMASK, &empty, NULL));
+	TEST_ASSERT_EQUAL_HEX32(0, handler_haveSignal);
+	TEST_ASSERT_NOT_EQUAL(SIG_ERR, signal(SIGUSR1, SIG_DFL));
+	TEST_ASSERT_EQUAL_HEX32(0, handler_haveSignal);
+}
+
+
+TEST(sigaction, unmask_changed_action_default_to_handler)
+{
+	sigset_t masked, empty;
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&empty));
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&masked));
+	TEST_ASSERT_EQUAL_INT(0, sigaddset(&masked, SIGUSR1));
+
+	TEST_ASSERT_EQUAL_INT(0, sigprocmask(SIG_SETMASK, &masked, NULL));
+	TEST_ASSERT_NOT_EQUAL(SIG_ERR, signal(SIGUSR1, SIG_DFL));
+	TEST_ASSERT_EQUAL_INT(0, raise(SIGUSR1));
+	TEST_ASSERT_NOT_EQUAL(SIG_ERR, signal(SIGUSR1, sighandler));
+	TEST_ASSERT_EQUAL_INT(0, sigprocmask(SIG_SETMASK, &empty, NULL));
+	TEST_ASSERT_EQUAL_HEX32((1u << SIGUSR1), handler_haveSignal);
+}
+
+
+TEST(sigaction, unmask_changed_action_handler_to_default_ignored)
+{
+	sigset_t masked, empty;
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&empty));
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&masked));
+	TEST_ASSERT_EQUAL_INT(0, sigaddset(&masked, SIGURG));
+
+	TEST_ASSERT_EQUAL_INT(0, sigprocmask(SIG_SETMASK, &masked, NULL));
+	TEST_ASSERT_NOT_EQUAL(SIG_ERR, signal(SIGURG, sighandler));
+	TEST_ASSERT_EQUAL_INT(0, raise(SIGURG));
+	TEST_ASSERT_NOT_EQUAL(SIG_ERR, signal(SIGURG, SIG_DFL));
+	TEST_ASSERT_EQUAL_INT(0, sigprocmask(SIG_SETMASK, &empty, NULL));
+	TEST_ASSERT_EQUAL_HEX32(0, handler_haveSignal);
+	TEST_ASSERT_NOT_EQUAL(SIG_ERR, signal(SIGURG, sighandler));
+	TEST_ASSERT_EQUAL_HEX32(0, handler_haveSignal);
+}
+
+
+TEST(sigaction, unmask_changed_action_handler_to_default)
+{
+	sigset_t masked, empty;
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&empty));
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&masked));
+	TEST_ASSERT_EQUAL_INT(0, sigaddset(&masked, SIGUSR1));
+
+	pid_t pid = safe_fork();
+	TEST_ASSERT_GREATER_OR_EQUAL(0, pid);
+	if (pid > 0) {
+		int code;
+		TEST_ASSERT_EQUAL_INT(pid, wait(&code));
+		TEST_ASSERT_EQUAL_INT(true, WIFSIGNALED(code));
+	}
+	else {
+		handler_haveSignal = 0u;
+		sigprocmask(SIG_SETMASK, &masked, NULL);
+		signal(SIGUSR1, sighandler);
+		raise(SIGUSR1);
+		signal(SIGUSR1, SIG_DFL);
+		sigprocmask(SIG_SETMASK, &empty, NULL);
+		/*
+		 * POSIX: after pthread_sigmask() changes the currently blocked set of signals it shall determine
+		 * whether there are any pending unblocked signals; if there are any, then at least one of those signals
+		 * shall be delivered before the call to pthread_sigmask() returns
+		 */
+
+		_exit(0);
+	}
+}
+
+
+TEST(sigaction, handler_recursion_direct)
+{
+	sigset_t empty;
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&empty));
+
+	struct sigaction act = {
+		.sa_handler = sighandlerRecursive,
+		.sa_flags = 0,
+		.sa_mask = empty,
+	};
+	TEST_ASSERT_EQUAL_INT(0, sigaction(SIGUSR1, &act, NULL));
+	TEST_ASSERT_EQUAL_INT(0, raise(SIGUSR1));
+	TEST_ASSERT_EQUAL_INT(0, handler_countdown);
+}
+
+
+TEST(sigaction, handler_recursion_raise)
+{
+	sigset_t set;
+	sigset_t empty;
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&empty));
+
+	struct sigaction act = {
+		.sa_handler = sighandlerReraise,
+		.sa_flags = 0,
+		.sa_mask = empty,
+	};
+
+	TEST_ASSERT_EQUAL_INT(0, sigaction(SIGUSR1, &act, NULL));
+	TEST_ASSERT_EQUAL_INT(0, raise(SIGUSR1));
+	while (handler_countdown > 0) {
+		TEST_ASSERT_EQUAL_INT(-1, sigsuspend(&empty));
+	}
+	TEST_ASSERT_EQUAL_INT(0, handler_countdown);
+	set = handler_sigset;
+	TEST_ASSERT_EQUAL_INT(1, sigismember(&set, SIGUSR1));
+}
+
+
+TEST(sigaction, handler_recursion_raise_nodefer)
+{
+	sigset_t set;
+	sigset_t empty;
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&empty));
+
+	struct sigaction act = {
+		.sa_handler = sighandlerReraise,
+		.sa_flags = SA_NODEFER,
+		.sa_mask = empty,
+	};
+
+	TEST_ASSERT_EQUAL_INT(0, sigaction(SIGUSR1, &act, NULL));
+	TEST_ASSERT_EQUAL_INT(0, raise(SIGUSR1));
+	TEST_ASSERT_EQUAL_INT(0, handler_countdown);
+	set = handler_sigset;
+	TEST_ASSERT_EQUAL_INT(0, sigismember(&set, SIGUSR1));
+}
+
+
+TEST(sigaction, sigaction_in_handler_handle)
+{
+	sigset_t empty;
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&empty));
+
+	struct sigaction action = {
+		.sa_handler = sighandlerAction,
+		.sa_flags = 0,
+		.sa_mask = empty,
+	};
+	handler_sigaction.sa_handler = sighandler;
+	handler_sigaction.sa_flags = 0;
+	handler_sigaction.sa_mask = empty;
+	TEST_ASSERT_EQUAL_INT(0, sigaction(SIGUSR2, &action, NULL));
+	TEST_ASSERT_EQUAL_INT(0, raise(SIGUSR2));
+	TEST_ASSERT_EQUAL_HEX32(0u, handler_haveSignal);
+	TEST_ASSERT_EQUAL_INT(0, raise(SIGUSR2));
+	TEST_ASSERT_EQUAL_HEX32((1u << SIGUSR2), handler_haveSignal);
+}
+
+
+TEST(sigaction, sigaction_in_handler_handle_reraise)
+{
+	sigset_t empty;
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&empty));
+
+	handler_sigaction.sa_handler = sighandler;
+	handler_sigaction.sa_flags = 0;
+	handler_sigaction.sa_mask = empty;
+	struct sigaction action = {
+		.sa_handler = sighandlerAction,
+		.sa_flags = 0,
+		.sa_mask = empty,
+	};
+	TEST_ASSERT_EQUAL_INT(0, sigaction(SIGUSR1, &action, NULL));
+	TEST_ASSERT_EQUAL_INT(0, raise(SIGUSR1));
+	if (handler_haveSignal == 0u) {
+		TEST_ASSERT_EQUAL_INT(-1, sigsuspend(&empty));
+	}
+	TEST_ASSERT_EQUAL_HEX32((1u << SIGUSR1), handler_haveSignal);
+}
+
+
+TEST(sigaction, sigaction_in_handler_nodefer_handle_reraise)
+{
+	sigset_t empty;
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&empty));
+
+	handler_sigaction.sa_handler = sighandler;
+	handler_sigaction.sa_flags = 0;
+	handler_sigaction.sa_mask = empty;
+	struct sigaction actionNodefer = {
+		.sa_handler = sighandlerAction,
+		.sa_flags = SA_NODEFER,
+		.sa_mask = empty,
+	};
+	TEST_ASSERT_EQUAL_INT(0, sigaction(SIGUSR1, &actionNodefer, NULL));
+	TEST_ASSERT_EQUAL_INT(0, raise(SIGUSR1));
+	TEST_ASSERT_EQUAL_HEX32((1u << SIGUSR1), handler_haveSignal);
+}
+
+
+TEST(sigaction, sigaction_in_handler_ignore)
+{
+	sigset_t empty;
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&empty));
+
+	handler_sigaction.sa_handler = SIG_IGN;
+	handler_sigaction.sa_flags = 0;
+	handler_sigaction.sa_mask = empty;
+	TEST_ASSERT_NOT_EQUAL(SIG_ERR, signal(SIGUSR1, &sighandlerAction));
+	TEST_ASSERT_EQUAL_INT(0, raise(SIGUSR1));
+	TEST_ASSERT_EQUAL_INT(0, raise(SIGUSR1));
+	TEST_ASSERT_EQUAL_INT(0, raise(SIGUSR1));
+	TEST_ASSERT_EQUAL_PTR(SIG_IGN, signal(SIGUSR1, SIG_IGN));
+}
+
+
+TEST(sigaction, sigaction_in_handler_default)
+{
+	sigset_t empty;
+	TEST_ASSERT_EQUAL_INT(0, sigemptyset(&empty));
+
+	struct sigaction actionNodefer = {
+		.sa_handler = sighandlerAction,
+		.sa_flags = SA_NODEFER,
+		.sa_mask = empty,
+	};
+
+	pid_t pid = safe_fork();
+	TEST_ASSERT_GREATER_OR_EQUAL(0, pid);
+	if (pid > 0) {
+		int code;
+		TEST_ASSERT_EQUAL_INT(pid, wait(&code));
+		TEST_ASSERT_EQUAL_INT(true, WIFSIGNALED(code));
+		TEST_ASSERT_EQUAL_INT(SIGUSR1, WTERMSIG(code));
+	}
+	else {
+		handler_sigaction.sa_handler = SIG_DFL;
+		handler_sigaction.sa_flags = 0;
+		handler_sigaction.sa_mask = empty;
+		sigaction(SIGUSR1, &actionNodefer, NULL);
+		raise(SIGUSR1);
+		_exit(0);
+	}
+}
+
+
+TEST_GROUP_RUNNER(sigaction)
+{
+	RUN_TEST_CASE(sigaction, signal_termination_statuscode);
+	RUN_TEST_CASE(sigaction, signal_default_ignored);
+
+	RUN_TEST_CASE(sigaction, unmask_changed_action_handler_to_ignore);
+	RUN_TEST_CASE(sigaction, unmask_changed_action_default_to_ignore);
+	RUN_TEST_CASE(sigaction, unmask_changed_action_default_to_handler);
+	RUN_TEST_CASE(sigaction, unmask_changed_action_handler_to_default_ignored);
+	RUN_TEST_CASE(sigaction, unmask_changed_action_handler_to_default);
+
+	/*
+	 * Unmasking with initial SIG_IGN is omitted, as:
+	 * POSIX: setting sigaction to SIG_IGN can release pending signal
+	 */
+
+	RUN_TEST_CASE(sigaction, handler_recursion_direct);
+	RUN_TEST_CASE(sigaction, handler_recursion_raise);
+	RUN_TEST_CASE(sigaction, handler_recursion_raise_nodefer);
+
+	RUN_TEST_CASE(sigaction, sigaction_in_handler_handle);
+	RUN_TEST_CASE(sigaction, sigaction_in_handler_handle_reraise);
+	RUN_TEST_CASE(sigaction, sigaction_in_handler_nodefer_handle_reraise);
+	RUN_TEST_CASE(sigaction, sigaction_in_handler_ignore);
+	RUN_TEST_CASE(sigaction, sigaction_in_handler_default);
 }
