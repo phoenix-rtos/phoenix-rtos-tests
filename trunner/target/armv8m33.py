146199cac02041e43664d48c1c3f42e6840d1e6a
Damian Modzelewski <Hootwheels989@gmail.com>
2025-10-23 14:54:52 +0200

    armv8m33: split MCXN94x target for tests on CPU0/1
    JIRA: CI-592

--- trunner/target/armv8m33.py
+++ trunner/target/armv8m33.py
@@ -71,8 +71,6 @@ class MCXN947PloAppLoader(TerminalHarness, PloInterface):
 
 
 class MCXN94xTarget(TargetBase):
-    name = "armv8m33-mcxn94x-frdm"
-    rootfs = False
     image_file = "phoenix.disk"
 
     def __init__(self, host: Host, port: Optional[str] = None, baudrate: int = 115200):
@@ -134,3 +132,13 @@ class MCXN94xTarget(TargetBase):
         builder.add(test.harness)
 
         return builder.get_harness()
+
+
+class MCXN94xCPU0Target(MCXN94xTarget):
+    name = "armv8m33-mcxn94x-frdm"
+    rootfs = False
+
+
+class MCXN94xCPU1Target(MCXN94xTarget):
+    name = "armv8m33-mcxn94x-frdm_cpu1"
+    rootfs = False
