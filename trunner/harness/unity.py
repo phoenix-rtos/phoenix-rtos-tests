84912119ae7bfcd53f04646b727e45c5a62ed852
Adam Debek <adam.debek@phoenix-rtos.com>
2025-12-10 20:10:18 +0100

    trunner: extend unity harness for network tests
    JIRA: CI-343

--- trunner/harness/unity.py
+++ trunner/harness/unity.py
@@ -1,8 +1,59 @@
+import socket
+import os
+import re
+
 from typing import Optional
 
 from trunner.ctx import TestContext
 from trunner.dut import Dut
 from trunner.types import Status, TestResult
+
+
+class NetworkTestResponder:
+    SOCKET_TIMEOUT = 10
+    PORT = 50000
+    peer_ip = None
+    sock = None
+    data = None
+
+    @staticmethod
+    def _get_peer_ip() -> str:
+        iface_config = os.environ.get("IFACE_CONFIG")
+        match = re.search(r"\d{1,3}(\.\d{1,3}){3}", iface_config)
+        return match.group()
+
+    @classmethod
+    def do_cmd(cls, cmd: str):
+        if cmd == "Accept":
+            cls.peer_ip = cls._get_peer_ip()
+            cls.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            cls.sock.settimeout(cls.SOCKET_TIMEOUT)
+            cls.sock.connect((cls.peer_ip, cls.PORT))
+
+        elif cmd == "Connect":
+            listen_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            listen_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            listen_sock.settimeout(cls.SOCKET_TIMEOUT)
+            listen_sock.bind(("0.0.0.0", cls.PORT))
+            listen_sock.listen()
+            cls.sock, _ = listen_sock.accept()
+            listen_sock.close()
+
+        elif cmd == "Close":
+            cls.sock.close()
+
+        elif cmd.startswith("Transmit"):
+            size = int(re.search(r"\d+", cmd).group())
+
+            if cls.data is None:
+                cls.data = bytearray()
+
+            cls.data += cls.sock.recv(size, socket.MSG_WAITALL)
+
+        elif cmd.startswith("Receive"):
+            size = int(re.search(r"\d+", cmd).group())
+            cls.sock.sendall(cls.data[:size])
+            cls.data = cls.data[size:]
 
 
 def unity_harness(dut: Dut, ctx: TestContext, result: TestResult) -> Optional[TestResult]:
@@ -11,6 +62,7 @@ def unity_harness(dut: Dut, ctx: TestContext, result: TestResult) -> Optional[Te
     # Fail need to have its own regex due to greedy matching
     result_fail_re = r"TEST\((?P<group>\w+), (?P<name>\w+)\) (?P<status>FAIL) at (?P<path>.*?):(?P<line>\d+)\r"
     final_re = r"(?P<total>\d+) Tests (?P<fail>\d+) Failures (?P<ignore>\d+) Ignored \r+\n(?P<result>OK|FAIL)"
+    network_re = r"NET: (?P<cmd>[^\r\n]+)\r?\n"
 
     last_assertion = {}
     stats = {"FAIL": 0, "IGNORE": 0, "PASS": 0}
@@ -21,12 +73,13 @@ def unity_harness(dut: Dut, ctx: TestContext, result: TestResult) -> Optional[Te
         timeout_val = 60
 
     while True:
-        idx = dut.expect([assert_re, result_re, result_fail_re, final_re], timeout=timeout_val)
+        idx = dut.expect([assert_re, result_re, result_fail_re, final_re, network_re], timeout=timeout_val)
         parsed = dut.match.groupdict()
 
         if idx == 0:
             if parsed["status"] in ["FAIL", "IGNORE"]:
                 last_assertion = parsed
+
         elif idx in (1, 2):
             if last_assertion.get("msg"):
                 parsed["msg"] = last_assertion["msg"]
@@ -40,6 +93,7 @@ def unity_harness(dut: Dut, ctx: TestContext, result: TestResult) -> Optional[Te
 
             stats[parsed["status"]] += 1
             results.append(parsed)
+
         elif idx == 3:
             for k, v in parsed.items():
                 if k != "result":
@@ -56,6 +110,9 @@ def unity_harness(dut: Dut, ctx: TestContext, result: TestResult) -> Optional[Te
                         f"{sum(stats.values())}, {stats['FAIL']}, {stats['IGNORE']}"))
 
             break
+
+        elif idx == 4:
+            NetworkTestResponder.do_cmd(parsed["cmd"])
 
     status = Status.FAIL if stats["FAIL"] != 0 else Status.OK
     return TestResult(status=status)
