9c3b04b22b7a674ebf33553394231230596c6254
Łukasz Kruszyński <lukasz.kruszynski@phoenix-rtos.com>
2025-12-23 14:21:06 +0100

    sample/pytest: move session-scope fixture to test-pytest.py and new function-scope fixture
    JIRA: CI-614

--- sample/pytest/conftest.py
+++ sample/pytest/conftest.py
@@ -32,17 +32,6 @@ class FakeDUT:
 
 
 @pytest.fixture(scope="session")
-def global_session_resource():
-    resource = {"status": "ready", "id": 999}
-    print("\n\t[SESSION] :::: STARTING\n")
-    
-    yield resource
-
-    print("\n\t[SESSION] :::: TEARING DOWN\n")
-    resource["status"] = "closed"
-
-
-@pytest.fixture(scope="session")
 def fake_dut_session():
     dut = FakeDUT()
     
