fcead494a926aeffcc6bbb0e74500bbf4eda9fa3
Władysław Młynik <wladyslaw.mlynik@phoenix-rtos.com>
2025-12-31 10:36:39 +0100

    trunner: add armv8m55-stm32n6-nucleo target
    JIRA: CI-598

--- trunner/harness/__init__.py
+++ trunner/harness/__init__.py
@@ -12,6 +12,7 @@ from .base import (
     Rebooter,
 )
 from .plo import (
+    PloError,
     PloInterface,
     PloJffs2CleanmarkerSpec,
     PloPhoenixdSyspageLoader,
@@ -39,6 +40,7 @@ __all__ = [
     "Rebooter",
     "RebooterHarness",
     "TestStartRunningHarness",
+    "PloError",
     "PloImageLoader",
     "PloPhoenixdSyspageLoader",
     "PloRamSyspageLoader",
