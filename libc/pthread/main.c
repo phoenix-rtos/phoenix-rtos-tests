f6c1565d27d590505890abede1987aa7a7ec5dab
Damian Loewnau <damian.loewnau@phoenix-rtos.com>
2026-05-20 13:49:12 +0200

    posix-test-implement: add more pthread tests for specific pthread functions
    Tests added for following functions:
    
    pthread_atfork()
    
    pthread_attr_destroy
    
    pthread_attr_getdetachstate()
    
    pthread_attr_getschedparam()
    
    pthread_attr_getschedpolicy()
    
    pthread_attr_getscope()
    
    pthread_attr_getstack()
    
    pthread_attr_getstacksize()
    
    pthread_attr_init()
    
    pthread_attr_setdetachstate()
    
    pthread_attr_setschedparam()
    
    pthread_attr_setschedpolicy()
    
    pthread_attr_setstacksize()
    
    pthread_cancel()
    
    pthread_cleanup_pop()
    
    pthread_cleanup_push()
    
    pthread_cond_broadcast()
    
    pthread_cond_destroy()
    
    pthread_cond_signal()
    
    pthread_condattr_destroy()
    
    pthread_condattr_init()
    
    pthread_condattr_setclock()
    
    pthread_detach()
    
    pthread_equal()
    
    pthread_exit()
    
    pthread_getspecific()
    
    pthread_join()
    
    pthread_key_create()
    
    pthread_key_delete()
    
    pthread_mutex_destroy()
    
    pthread_mutex_init()
    
    pthread_mutex_lock()
    
    pthread_mutex_trylock()
    
    pthread_mutex_unlock()
    
    pthread_mutexattr_destroy()
    
    pthread_mutexattr_gettype()
    
    pthread_mutexattr_init()
    
    pthread_mutexattr_settype()
    
    pthread_once()
    
    pthread_self()
    
    pthread_setcancelstate()
    
    pthread_setspecific()
    
    Assisted by claude-opus-4.6

--- libc/pthread/main.c
+++ libc/pthread/main.c
@@ -5,27 +5,44 @@
  *
  * Main entry point.
  *
- * Copyright 2023 Phoenix Systems
- * Author: Mateusz Bloch
+ * Copyright 2023, 2026 Phoenix Systems
+ * Author: Mateusz Bloch, Damian Loewnau
  *
  * This file is part of Phoenix-RTOS.
  *
  * %LICENSE%
  */
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 #include "unity_fixture.h"
 
-/* no need for forward declarations, RUN_TEST_GROUP does it by itself */
 void runner(void)
 {
 	RUN_TEST_GROUP(test_pthread_cond);
 	RUN_TEST_GROUP(test_pthread_cleanup);
+	RUN_TEST_GROUP(pthread_attr);
+	RUN_TEST_GROUP(pthread_mutex);
+	RUN_TEST_GROUP(pthread_mutexattr);
+	RUN_TEST_GROUP(pthread_lifecycle);
+	RUN_TEST_GROUP(pthread_once);
+	RUN_TEST_GROUP(pthread_key);
+	RUN_TEST_GROUP(pthread_cancel);
+	RUN_TEST_GROUP(pthread_atfork);
 }
 
 
 int main(int argc, char *argv[])
 {
+	const char *var = "POSIXLY_CORRECT";
+
+	if (setenv(var, "y", 1) != 0) {
+		fprintf(stderr, "Setting %s environment variable failed: %s\n", var, strerror(errno));
+		return 1;
+	}
+
 	return (UnityMain(argc, (const char **)argv, runner) == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
